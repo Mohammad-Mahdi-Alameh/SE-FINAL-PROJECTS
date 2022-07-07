a4dcc9df71ddcf21d5ffba4a66806d3714db5aa4
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-07 22:21:14 +0300

    Backend : Generating a migration to create the "infrastuctural_probems" table

---
+++ WatchOut-Backend-Laravel/database/migrations/2022_07_07_191959_create_infrastructural_problems_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInfrastructuralProblemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('infrastructural_problems', function (Blueprint $table) {
+            $table->id();
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->integer('is_fixed');
+            $table->integer('risk_level');
+            $table->unsignedBigInteger('type_id');
+            $table->foreign('type_id')->references('id')->on('types');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('infrastructural_problems');
+    }
+}
