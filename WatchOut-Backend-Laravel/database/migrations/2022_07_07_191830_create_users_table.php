0dd27cb186dba5123c99904dab75b413aa118ede
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-07 22:19:24 +0300

    Backend : Generating a migration to create the "users" table

--- WatchOut-Backend-Laravel/database/migrations/2022_07_07_191830_create_users_table.php
+++ WatchOut-Backend-Laravel/database/migrations/2022_07_07_191830_create_users_table.php
@@ -15,11 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('username');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('score');
+            $table->string('level');
+            $table->string('is_admin');
             $table->timestamps();
         });
     }
