53192ee7a460dbd4993615614218c19ca45cec78
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-07 22:16:12 +0300

    Backend : versioning the APIs

--- WatchOut-Backend-Laravel/app/Providers/RouteServiceProvider.php
+++ WatchOut-Backend-Laravel/app/Providers/RouteServiceProvider.php
@@ -38,14 +38,25 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::prefix('api')
-                ->middleware('api')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+        });
+        
+        Route::group([
+            'middleware' => ['api', 'api_version:v1'],
+            'namespace'  => "{$this->apiNamespace}\V1",
+            'prefix'     => 'api/v1',
+        ], function ($router) {
+            require base_path('routes/api_v1.php');
+        });    
+        Route::group([
+            'middleware' => ['api', 'api_version:v2'],
+            'namespace'  => "{$this->apiNamespace}\V2",
+            'prefix'     => 'api/v2',
+        ], function ($router) {
+            require base_path('routes/api_v2.php');
         });
     }
 
@@ -60,4 +71,6 @@ class RouteServiceProvider extends ServiceProvider
             return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
         });
     }
+     /** @var string $apiNamespace */
+     protected $apiNamespace ='App\Http\Controllers\Api';
 }
--- WatchOut-Backend-Laravel/config/app.php
+++ WatchOut-Backend-Laravel/config/app.php
@@ -231,5 +231,13 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 
     ],
+     /*
+    |-------------------------------------------
+    | Current API Version
+    |-------------------------------------------
+    | That is the default API version of your API (Last version).
+    | The idea is that if there is no version when calling the API, it  | will be used this one.
+    */
+    'api_latest'  => '1',
 
 ];
