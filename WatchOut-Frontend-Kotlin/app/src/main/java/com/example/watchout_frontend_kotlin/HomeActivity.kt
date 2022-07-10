001202cf61eb90aa5a0ea1ff4fd3f188d6b2bdd9
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-10 19:04:34 +0300

    Frontend : Creating a toolbar displaying the name of the app

--- WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/HomeActivity.kt
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/HomeActivity.kt
@@ -2,10 +2,12 @@ package com.example.watchout_frontend_kotlin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setSupportActionBar(toolbar)
     }
 }
