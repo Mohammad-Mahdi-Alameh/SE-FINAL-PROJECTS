175f806a0d5ee28cbd8e5afb61a7ae6d15a1f983
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-11 00:21:39 +0300

    Frontend : Controlling the fragments on the screen by the bottom navigation bar (where every button view its corresponding fragment)

--- WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/HomeActivity.kt
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.example.watchout_frontend_kotlin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
@@ -9,5 +10,28 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setSupportActionBar(toolbar)
+        loadFragment(HomeFragment())
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {loadFragment(HomeFragment())}
+                R.id.profile -> loadFragment(ProfileFragment())
+                R.id.map -> loadFragment(MapFragment())
+
+            else -> {
+            }
+            }
+
+
+            true
+        }
     }
-}
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    }
+
+
