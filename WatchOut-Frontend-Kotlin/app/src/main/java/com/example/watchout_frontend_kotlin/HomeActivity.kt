e917fb3661691ee99d57c504b4e33200801f86b5
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-13 01:30:39 +0300

    Frontend : Refactoring the navigation between the three fragments by using the navigation component

--- WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/HomeActivity.kt
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/HomeActivity.kt
@@ -2,7 +2,8 @@ package com.example.watchout_frontend_kotlin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
@@ -10,29 +11,9 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setSupportActionBar(toolbar)
-        loadFragment(HomeFragment())
-        bottomNav.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.home -> {loadFragment(HomeFragment())}
-                R.id.profile -> loadFragment(ProfileFragment())
-                R.id.map -> loadFragment(MapFragment())
-
-            else -> {
-            }
-            }
-
-
-            true
-        }
-    }
-    private  fun loadFragment(fragment: Fragment){
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.container,fragment)
-        transaction.addToBackStack(null)
-        transaction.commit()
-    }
-
+        val navController = findNavController(R.id.fragment_container_view_tag)
+        bottomNav.setupWithNavController(navController)
 
     }
-
+}
 
