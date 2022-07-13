36b4fa3f38aa186aebb1f4b2ebe94dae9583a6fa
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-14 00:42:47 +0300

    Frontend : Setting "signUp" post method's request as the signupInfo instance , and the response as the signupResponse instance

--- WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/api/RestApiService.kt
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/api/RestApiService.kt
@@ -3,6 +3,8 @@ package com.example.watchout_frontend_kotlin.api
 import android.util.Log
 import com.example.watchout_frontend_kotlin.model.LoginInfo
 import com.example.watchout_frontend_kotlin.model.LoginResponse
+import com.example.watchout_frontend_kotlin.model.SignupInfo
+import com.example.watchout_frontend_kotlin.model.SignupResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -24,6 +26,24 @@ class RestApiService {
                         response.errorBody()?.let { Log.e("error", it.string()) }
                     }
                     Log.d("User",response.toString())
+                }
+            }
+        )
+    }
+    fun signUp(signupInfo: SignupInfo, onResult: (SignupResponse?) -> Unit){
+        val retrofit = ServiceBuilder.buildService(LaravelRestApi::class.java)
+        retrofit.signUp(signupInfo).enqueue(
+            object : Callback<SignupResponse> {
+                override fun onFailure(call: Call<SignupResponse>, t: Throwable) {
+                    Log.i("error","error in getting response")
+                    onResult(null)
+                }
+                override fun onResponse( call: Call<SignupResponse>, response: Response<SignupResponse>) {
+                    val addedUser = response.body()
+                    Log.d("Added User",response.body().toString())
+                    onResult(addedUser)
+
+
                 }
             }
         )
