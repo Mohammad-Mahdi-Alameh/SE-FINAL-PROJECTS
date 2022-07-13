7bb34f06ea8c97333e5b533e6d018d0fa29f61f0
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-14 00:06:34 +0300

    Frontend : Creating "logIn" post method in the LaravelRestApi interface

--- WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/api/LaravelRestApi.kt
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/api/LaravelRestApi.kt
@@ -1,6 +1,16 @@
 package com.example.watchout_frontend_kotlin.api
 
+import com.example.watchout_frontend_kotlin.model.LoginInfo
+import com.example.watchout_frontend_kotlin.model.LoginResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
 interface LaravelRestApi {
 
+    @Headers("Content-Type: application/json")
+    @POST("/api/v1/user/login")
+    fun logIn(@Body loginInfo: LoginInfo): Call<LoginResponse>
 
 }
