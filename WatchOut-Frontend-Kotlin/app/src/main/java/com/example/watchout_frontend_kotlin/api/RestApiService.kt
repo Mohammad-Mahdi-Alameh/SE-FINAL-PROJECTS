148f89912dbc1beb295dfd6367320124f61dd4bf
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-14 00:13:23 +0300

    Frontend : Setting "logIn" post method's request as the loginInfo instance , and the response as the loginResponse instance

--- WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/api/RestApiService.kt
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/api/RestApiService.kt
@@ -1,5 +1,31 @@
 package com.example.watchout_frontend_kotlin.api
 
+import android.util.Log
+import com.example.watchout_frontend_kotlin.model.LoginInfo
+import com.example.watchout_frontend_kotlin.model.LoginResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 class RestApiService {
 
+    fun login(loginInfo: LoginInfo, onResult: (LoginResponse?) -> Unit){
+        val retrofit = ServiceBuilder.buildService(LaravelRestApi::class.java)
+        retrofit.logIn(loginInfo).enqueue(
+            object : Callback<LoginResponse> {
+                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                    Log.i("error","error in getting response")
+                    onResult(null)
+                }
+                override fun onResponse( call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                    val addedUser = response.body()
+                    onResult(addedUser)
+                    if (response.code() == 400) {
+                        response.errorBody()?.let { Log.e("error", it.string()) }
+                    }
+                    Log.d("User",response.toString())
+                }
+            }
+        )
+    }
 }
