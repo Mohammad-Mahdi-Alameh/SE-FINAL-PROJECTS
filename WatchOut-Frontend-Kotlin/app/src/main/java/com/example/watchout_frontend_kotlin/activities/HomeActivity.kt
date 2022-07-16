39964e394282e88dab42e474ff45da54f185b141
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-17 00:28:40 +0300

    Frontend : Finishing the linking of the getNearInfras function to the database using Retrofit2 and triggering it in the onCreate of HomeActivity( temporary  base_longitude will be  "-98.56586" and base_latitude will be "29.46786" (as if they are the lat and long of home right  now )because live location isn't activated yet)

--- WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/activities/HomeActivity.kt
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/activities/HomeActivity.kt
@@ -1,11 +1,15 @@
 package com.example.watchout_frontend_kotlin.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.watchout_frontend_kotlin.R
+import com.example.watchout_frontend_kotlin.api.RestApiService
+import com.example.watchout_frontend_kotlin.models.GetNearInfrasInfo
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.gson.Gson
 
 //import kotlinx.android.synthetic.main.activity_home.*
 
@@ -17,6 +21,21 @@ class HomeActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
         val navController = findNavController(R.id.fragment_container_view_tag)
         bottomNav.setupWithNavController(navController)
+
+        //infras:Infrastructural problems
+        val apiService = RestApiService()
+        val getNearInfrasInfo = GetNearInfrasInfo(
+            base_latitude = "29.46786",
+            base_longitude = "-98.56586")
+
+        apiService.getNearInfras(getNearInfrasInfo) {
+            if (it?.size != null) {
+                Log.i("Near Infras", Gson().toJson(it))
+
+            } else {
+                Log.i("Error", "Error")
+            }
+        }
 
     }
 }
