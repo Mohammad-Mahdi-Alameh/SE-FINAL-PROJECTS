eccedd305f158adfaad5010fb19d9925daea0df0
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-16 16:56:11 +0300

    Backend : Creating getDistance function in the get method in UserController that calculates the distance that takes 2 points with their long and lat and return the distance in km

--- WatchOut-Backend-Laravel/app/Http/Controllers/Api/V1/User/UserController.php
+++ WatchOut-Backend-Laravel/app/Http/Controllers/Api/V1/User/UserController.php
@@ -14,26 +14,27 @@ class UserController extends Controller
     public function report(Request $request)
     {
 
-
+        
         $validator = Validator::make($request->all(), [
-            'latitude' => 'string|min:2|max:100',
-            'longitude' => 'string|min:2|max:100',
-            'type' => 'required|string',
+            'latitude' => 'required|string|min:2|max:100',
+            'longitude' => 'required|string|min:2|max:100',
+            // 'risk_level' => 'required',
+            'type' => 'required|string', 
         ]);
 
         if($validator->fails()) {
             return response()->json(["message" => "Validator Failed ! Check your submiited values again!"]);
         }
 
-
+     
         $infrastructural_problem = new InfrastructuralProblem;
 
         $record = Type::where("name","=",$request->type)->get();
-
+        
         if(count($record) == 0){
             return response()->json([
                 'message' => 'There is no such type!',
-
+                
             ]);
         }
         $type_id = json_decode($record,true)[0]["id"];
@@ -41,6 +42,7 @@ class UserController extends Controller
         $infrastructural_problem->latitude = $request->latitude;
         $infrastructural_problem->longitude = $request->longitude;
         $infrastructural_problem->is_fixed = 0;
+        $infrastructural_problem->risk_level = 0;
         $infrastructural_problem->type_id = $type_id;
 
         $infrastructural_problem->save();
@@ -50,8 +52,29 @@ class UserController extends Controller
         ], Response::HTTP_OK);
     }
 
-    public function get(){
+    public function get(Request $request){
+        function getDistance($lat1, $lon1, $lat2, $lon2) {
+            if (($lat1 == $lat2) && ($lon1 == $lon2)) {
+              return 0;
+            }
+            else {
+              $theta = $lon1 - $lon2;
+              $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+              $dist = acos($dist);
+              $dist = rad2deg($dist);
+              $miles = $dist * 60 * 1.1515;
+              $unit = strtoupper($unit);
+          
+              return $miles * 1.609344;
+             
+            }
+          }
+
         $infrastructural_problems=InfrastructuralProblem::all();
         return $infrastructural_problems;
     }
+   
+
+      
+      
 }
