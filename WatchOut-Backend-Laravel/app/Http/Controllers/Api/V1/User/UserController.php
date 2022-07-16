e453f82dd939cf6111e7645e006b8354161fc692
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-16 16:50:30 +0300

    Backend : Fixing some issues (spelling mistakes , removing unneeded code , fixing the validation of lat and long ) in the UserController

--- WatchOut-Backend-Laravel/app/Http/Controllers/Api/V1/User/UserController.php
+++ WatchOut-Backend-Laravel/app/Http/Controllers/Api/V1/User/UserController.php
@@ -14,27 +14,26 @@ class UserController extends Controller
     public function report(Request $request)
     {
 
-        
+
         $validator = Validator::make($request->all(), [
-            'latitude' => 'required|string|min:2|max:100|unique:infrastructural_problems',
-            'longitude' => 'required|string|min:2|max:100|unique:infrastructural_problems',
-            'risk_level' => 'required',
-            'type_name' => 'required|string', 
+            'latitude' => 'string|min:2|max:100',
+            'longitude' => 'string|min:2|max:100',
+            'type' => 'required|string',
         ]);
 
         if($validator->fails()) {
             return response()->json(["message" => "Validator Failed ! Check your submiited values again!"]);
         }
 
-     
+
         $infrastructural_problem = new InfrastructuralProblem;
 
-        $record = Type::where("name","=",$request->type_name)->get();
-        
+        $record = Type::where("name","=",$request->type)->get();
+
         if(count($record) == 0){
             return response()->json([
                 'message' => 'There is no such type!',
-                
+
             ]);
         }
         $type_id = json_decode($record,true)[0]["id"];
@@ -42,13 +41,12 @@ class UserController extends Controller
         $infrastructural_problem->latitude = $request->latitude;
         $infrastructural_problem->longitude = $request->longitude;
         $infrastructural_problem->is_fixed = 0;
-        $infrastructural_problem->risk_level = $request->risk_level;
         $infrastructural_problem->type_id = $type_id;
 
         $infrastructural_problem->save();
 
         return response()->json([
-            'message' => 'Infrastructural problem reported successfuly',
+            'message' => 'Infrastructural problem reported successfully',
         ], Response::HTTP_OK);
     }
 
