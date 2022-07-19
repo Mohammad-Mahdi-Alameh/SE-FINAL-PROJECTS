086916d09a88bcc51a853781dad4c9d72b3b6af3
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-19 18:47:20 +0300

    Frontend :  Creating getDistance function in HomeActivity that takes 2 points with their long and lat and return the distance in km

--- WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/activities/HomeActivity.kt
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/activities/HomeActivity.kt
@@ -164,5 +164,26 @@ class HomeActivity : AppCompatActivity() {
         }
 
     }
+    private fun getDistance(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
+        val theta = lon1 - lon2
+        var dist = (Math.sin(deg2rad(lat1))
+                * Math.sin(deg2rad(lat2))
+                + (Math.cos(deg2rad(lat1))
+                * Math.cos(deg2rad(lat2))
+                * Math.cos(deg2rad(theta))))
+        dist = Math.acos(dist)
+        dist = rad2deg(dist)
+        dist *= 60 * 1.1515
+        dist *= 1.609344   //transform dist from miles to km
+        return  dist
+    }
+
+    private fun deg2rad(deg: Double): Double {
+        return deg * Math.PI / 180.0
+    }
+
+    private fun rad2deg(rad: Double): Double {
+        return rad * 180.0 / Math.PI
+    }
 
 }
