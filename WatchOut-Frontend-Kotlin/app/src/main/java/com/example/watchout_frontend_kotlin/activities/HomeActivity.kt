23cff6bc12ba5ae2e397c2ba10b531859512f8ab
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-19 18:08:38 +0300

    Frontend : Creating trackCurrentLocation function (with prerequisite function setupLocClient() to let us activate tracking)that will track the users live location and save it in firebase realtime database whenever its changed (and if location permission wasn't granted it will request it)

--- WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/activities/HomeActivity.kt
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/activities/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.example.watchout_frontend_kotlin.activities
 
 import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -10,10 +11,16 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.watchout_frontend_kotlin.R
 import com.example.watchout_frontend_kotlin.api.RestApiService
 import com.example.watchout_frontend_kotlin.models.GetNearInfrasInfo
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.gson.Gson
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var fusedLocClient: FusedLocationProviderClient
+    private var database: FirebaseDatabase = FirebaseDatabase.getInstance()
     companion object {
         private const val REQUEST_LOCATION = 1 //request code to identify specific permission request
         private const val TAG = "HomeActivity" // for debugging
@@ -27,7 +34,13 @@ class HomeActivity : AppCompatActivity() {
         val navController = findNavController(R.id.fragment_container_view_tag)
         bottomNav.setupWithNavController(navController)
         //infras:Infrastructural problems
+
+
+
+        setupLocClient()
+        trackCurrentLocation()
         getNearInfras("29.46786","-98.56586")
+
         }
 
 
@@ -35,8 +48,7 @@ class HomeActivity : AppCompatActivity() {
         val apiService = RestApiService()
         val getNearInfrasInfo = GetNearInfrasInfo(
             base_latitude = latitude,
-            base_longitude = longitude
-        )
+            base_longitude = longitude)
         apiService.getNearInfras(getNearInfrasInfo) {
             if (it?.size != null) {
                 Log.i("Near Infras", Gson().toJson(it))
@@ -52,11 +64,52 @@ class HomeActivity : AppCompatActivity() {
         Log.i("result",data)
     }
 
+
+
+    private fun setupLocClient() {
+        fusedLocClient =
+            LocationServices.getFusedLocationProviderClient(this)
+    }
     private fun requestLocPermissions() {
         ActivityCompat.requestPermissions(this,
             arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), //permission in the manifest
             REQUEST_LOCATION
         )
     }
+
+
+    private fun trackCurrentLocation() {
+        // Check if the ACCESS_FINE_LOCATION permission was granted before requesting a location
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED) {
+
+            // call requestLocPermissions() if permission isn't granted
+            requestLocPermissions()
+        } else {
+
+            fusedLocClient.lastLocation.addOnCompleteListener {
+                // lastLocation is a task running in the background
+                val location = it.result //obtain location which contains longitude , latitude , speed , altitude and much more ......
+                //reference to the database
+                //val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+                val ref: DatabaseReference = database.getReference("test")
+                if (location != null) {
+
+
+                    //Save the location data to the database
+                    ref.setValue(location)
+                } else {
+                    // if location is null , log an error message
+                    Log.e("error", "No location found")
+                }
+
+
+
+            }
+        }
+    }
+
+
 
 }
