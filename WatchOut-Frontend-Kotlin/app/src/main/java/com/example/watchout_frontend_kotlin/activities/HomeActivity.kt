1f727c4fa79fb0668c601c53c8ce79c55ac8a9f9
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-21 01:11:09 +0300

    Frontend :  Refactoring the trackCurrentLocation function and changing the whole algorithm (using location manager and provider )

--- WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/activities/HomeActivity.kt
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/activities/HomeActivity.kt
@@ -1,7 +1,12 @@
 package com.example.watchout_frontend_kotlin.activities
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -25,8 +30,10 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var fusedLocClient: FusedLocationProviderClient
     private var database: FirebaseDatabase = FirebaseDatabase.getInstance()
     private var dbReference: DatabaseReference = database.getReference("test")
+
     companion object {
-        private const val REQUEST_LOCATION = 1 //request code to identify specific permission request
+        private const val REQUEST_LOCATION =
+            1 //request code to identify specific permission request
         private const val TAG = "HomeActivity" // for debugging
     }
 
@@ -38,23 +45,78 @@ class HomeActivity : AppCompatActivity() {
         val navController = findNavController(R.id.fragment_container_view_tag)
         bottomNav.setupWithNavController(navController)
         //infras:Infrastructural problems
-
-
-
         setupLocClient()
-        trackCurrentLocation()
+        trackCurrentLocation(this)
         dbReference = Firebase.database.reference
         dbReference.addValueEventListener(locListener)
-        getNearInfras("29.46786","-98.56586")
+        getNearInfras("29.46786", "-98.56586")
+    }
 
+    private fun trackCurrentLocation(context: Context?) {
+
+        //Check all location permission granted or not
+        if (ActivityCompat.checkSelfPermission(
+                context!!,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestLocPermissions()
+        } else {
+
+            //Criteria class indicating the application criteria for selecting a location provider
+            val criteria = Criteria()
+            criteria.accuracy = Criteria.ACCURACY_FINE
+            criteria.isSpeedRequired = true
+            criteria.speedAccuracy = Criteria.ACCURACY_HIGH
+
+            val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+            val provider = locationManager.getBestProvider(criteria, true)
+
+            if (provider != null) {
+                locationManager.requestLocationUpdates(
+                    provider, 1, 0.1f, object : LocationListener {
+                        override fun onLocationChanged(location: Location) {
+                            val ref: DatabaseReference = database.getReference("test")
+                            if (location != null) {
+                                ref.setValue(location)
+                            } else {
+                                // if location is null , log an error message
+                                Log.e("error", "No location found")
+                            }
+
+
+                        }
+
+                        override fun onProviderDisabled(provider: String) {
+                            //Provider disabled
+                        }
+
+                        override fun onProviderEnabled(provider: String) {
+                            //Provider enabled
+                        }
+
+                        override fun onStatusChanged(
+                            provider: String?,
+                            status: Int,
+                            extras: Bundle?
+                        ) {
+
+                        }
+
+                    })
+            }
         }
+    }
 
-
-    private fun getNearInfras(latitude : String , longitude : String) {
+    private fun getNearInfras(latitude: String, longitude: String) {
         val apiService = RestApiService()
         val getNearInfrasInfo = GetNearInfrasInfo(
             base_latitude = latitude,
-            base_longitude = longitude)
+            base_longitude = longitude
+        )
         apiService.getNearInfras(getNearInfrasInfo) {
             if (it?.size != null) {
                 Log.i("Near Infras", Gson().toJson(it))
@@ -63,74 +125,47 @@ class HomeActivity : AppCompatActivity() {
                 Log.i("Error", "Error")
 
             }
+        }
+
     }
 
-}
-    private fun showResult(data:String) {
-        Log.i("result",data)
+    private fun showResult(data: String) {
+        Log.i("result", data)
     }
-
 
 
     private fun setupLocClient() {
         fusedLocClient =
             LocationServices.getFusedLocationProviderClient(this)
     }
+
     private fun requestLocPermissions() {
-        ActivityCompat.requestPermissions(this,
+        ActivityCompat.requestPermissions(
+            this,
             arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), //permission in the manifest
             REQUEST_LOCATION
         )
     }
 
-
-    private fun trackCurrentLocation() {
-        // Check if the ACCESS_FINE_LOCATION permission was granted before requesting a location
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) !=
-            PackageManager.PERMISSION_GRANTED) {
-
-            // call requestLocPermissions() if permission isn't granted
-            requestLocPermissions()
-        } else {
-
-            fusedLocClient.lastLocation.addOnCompleteListener {
-                // lastLocation is a task running in the background
-                val location = it.result //obtain location which contains longitude , latitude , speed , altitude and much more ......
-                //reference to the database
-                //val database: FirebaseDatabase = FirebaseDatabase.getInstance()
-                val ref: DatabaseReference = database.getReference("test")
-                if (location != null) {
-
-
-                    //Save the location data to the database
-                    ref.setValue(location)
-                } else {
-                    // if location is null , log an error message
-                    Log.e("error", "No location found")
-                }
-
-
-
-            }
-        }
-    }
-
-
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
-        grantResults: IntArray) {
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         //check if the request code matches the REQUEST_LOCATION
-        if (requestCode == HomeActivity.REQUEST_LOCATION)
-        {
+        if (requestCode == HomeActivity.REQUEST_LOCATION) {
             //check if grantResults contains PERMISSION_GRANTED.If it does, call getCurrentLocation()
             if (grantResults.size == 1 && grantResults[0] ==
-                PackageManager.PERMISSION_GRANTED) {
-                trackCurrentLocation()
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                trackCurrentLocation(this)
             } else {
-                Toast.makeText(applicationContext, "Location permission was denied", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    applicationContext,
+                    "Location permission was denied",
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
     }
@@ -138,32 +173,32 @@ class HomeActivity : AppCompatActivity() {
     private val locListener = object : ValueEventListener {
         //     @SuppressLint("LongLogTag")
         override fun onDataChange(snapshot: DataSnapshot) {
-            if(snapshot.exists()){
+            if (snapshot.exists()) {
                 //get the exact longitude and latitude and speed from the database "test"
                 val location = snapshot.child("test").getValue(LocationInfo::class.java)
                 val locationLat = location?.latitude
                 val locationLong = location?.longitude
                 val speed = location?.speed
+
                 //trigger reading of location from database using the button
                 Log.i("lat", locationLat.toString())
                 Log.i("long", locationLong.toString())
                 Log.i("speed", speed.toString())
-//                getCurrentLocation()
-                    }
-                    else {
-                        // if location is null , log an error message
-                        Log.e(TAG, "user location cannot be found")
-                    }
-                }
-
+            } else {
+                // if location is null , log an error message
+                Log.e(TAG, "user location cannot be found")
+            }
+        }
 
 
         // show this toast if there is an error while reading from the database
         override fun onCancelled(error: DatabaseError) {
-            Toast.makeText(applicationContext, "Could not read from database", Toast.LENGTH_LONG).show()
+            Toast.makeText(applicationContext, "Could not read from database", Toast.LENGTH_LONG)
+                .show()
         }
 
     }
+
     private fun getDistance(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
         val theta = lon1 - lon2
         var dist = (Math.sin(deg2rad(lat1))
@@ -175,7 +210,7 @@ class HomeActivity : AppCompatActivity() {
         dist = rad2deg(dist)
         dist *= 60 * 1.1515
         dist *= 1.609344   //transform dist from miles to km
-        return  dist
+        return dist
     }
 
     private fun deg2rad(deg: Double): Double {
