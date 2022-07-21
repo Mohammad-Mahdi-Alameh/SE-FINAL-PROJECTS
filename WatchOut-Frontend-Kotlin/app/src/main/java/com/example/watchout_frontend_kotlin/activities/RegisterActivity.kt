aa6f445a8a2758a5edf4eb8e698702c11c17ee60
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-22 00:56:48 +0300

    Frontend :  Creating register activity with its corresponding layout

---
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/activities/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.watchout_frontend_kotlin.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.watchout_frontend_kotlin.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
