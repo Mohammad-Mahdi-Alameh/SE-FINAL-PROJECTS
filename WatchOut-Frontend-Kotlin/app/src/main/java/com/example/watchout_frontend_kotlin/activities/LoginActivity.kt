713875ad9d5b1f06d347bc96211dbb5e95bef3d9
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-21 21:28:47 +0300

    Frontend :  Creating login activity with its corresponding layout

---
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.watchout_frontend_kotlin.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.watchout_frontend_kotlin.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
