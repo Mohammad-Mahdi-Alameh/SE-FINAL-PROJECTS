2cba2be2c3d99bfca14fd512d86e15afe0dffe65
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-22 03:24:30 +0300

    Frontend :  Navigating from the sign in page to the register page when "Register" (in the login page) is clicked

--- WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/activities/LoginActivity.kt
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/activities/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.watchout_frontend_kotlin.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.watchout_frontend_kotlin.R
 
@@ -8,5 +10,12 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val register= findViewById<TextView>(R.id.navigateToRegister)
+        register.setOnClickListener{
+            val intent = Intent (this,RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
