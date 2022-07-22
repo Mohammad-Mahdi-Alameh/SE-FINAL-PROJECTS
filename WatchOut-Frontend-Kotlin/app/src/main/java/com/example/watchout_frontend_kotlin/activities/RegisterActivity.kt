0ef51f83d3c594311c956d2d230f9cd1124ed4c1
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-22 03:29:40 +0300

    Frontend :  Creating a toolbar in the register page with a backward button that will start a navigation to the login page when clicked

--- WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/activities/RegisterActivity.kt
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/activities/RegisterActivity.kt
@@ -1,6 +1,8 @@
 package com.example.watchout_frontend_kotlin.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.watchout_frontend_kotlin.R
 
@@ -8,5 +10,11 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val back_arrow = findViewById<ImageView>(R.id.back_arrow)
+        back_arrow.setOnClickListener {
+            startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+            finish()
+        }
     }
 }
