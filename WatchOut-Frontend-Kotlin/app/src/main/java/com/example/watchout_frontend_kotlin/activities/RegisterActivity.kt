ee1724a79c97d527fb4dee9002ddc83b56cafe5a
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-22 05:40:14 +0300

    Frontend :  Moving all code regarding the register process to the register activity(  algorithms and any other code related to register),and similarly all of this is due to the refactorization in the (UI/UX) design of the whole project

--- WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/activities/RegisterActivity.kt
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/activities/RegisterActivity.kt
@@ -2,9 +2,12 @@ package com.example.watchout_frontend_kotlin.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
+import android.util.Log
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.watchout_frontend_kotlin.R
+import com.example.watchout_frontend_kotlin.api.RestApiService
+import com.example.watchout_frontend_kotlin.models.SignupInfo
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +15,76 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
 
         val back_arrow = findViewById<ImageView>(R.id.back_arrow)
+        val login = findViewById<TextView>(R.id.navigateToLogin)
+        val register_btn = findViewById<Button>(R.id.register_btn)
         back_arrow.setOnClickListener {
-            startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
-            finish()
+            navigateToLogin()
+        }
+        login.setOnClickListener {
+            navigateToLogin()
+        }
+        register_btn.setOnClickListener {
+            register()
+        }
+
+    }
+
+    private fun navigateToLogin() {
+        startActivity(Intent(this@RegisterActivity, MainActivity::class.java))
+        finish()
+    }
+
+    private fun register() {
+        val firstname = findViewById<EditText>(R.id.first_name).text.toString()
+        val lastname = findViewById<EditText>(R.id.last_name).text.toString()
+        val username = findViewById<EditText>(R.id.new_username).text.toString()
+        val password = findViewById<EditText>(R.id.new_password).text.toString()
+        val confirmPassword = findViewById<EditText>(R.id.confirm_password).text.toString()
+        if (firstname.isEmpty() || lastname.isEmpty() || username.isEmpty() || password.isEmpty() ||
+            confirmPassword.isEmpty()
+        ) {
+            Toast.makeText(
+                applicationContext,
+                "Please fill the missing fields !",
+                Toast.LENGTH_SHORT
+            )
+                .show()
+        } else {
+            if (password != confirmPassword) {
+                Toast.makeText(
+                    applicationContext,
+                    "Entered passwords don't match !",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+            val apiService = RestApiService()
+            val signupInfo = SignupInfo(
+                firstname = firstname,
+                lastname = lastname,
+                username = username,
+                password = password,
+                c_password = confirmPassword
+            )
+
+            apiService.signUp(signupInfo) {
+                if (it?.message == "User successfully registered") {
+                    Log.i("message", "User added successfully")
+                } else {
+                    //Error on laravel validator because password < 6
+                    if (password.length < 6) {
+                        Log.i(
+                            "Signup Error",
+                            "Password too short ! It should be minimum six characters"
+                        )
+                    }
+                    //Error on laravel validator because username exists already
+                    else
+                        Log.i("Signup Error", "Username exists please choose another one !")
+                }
+            }
         }
     }
+
+
 }
