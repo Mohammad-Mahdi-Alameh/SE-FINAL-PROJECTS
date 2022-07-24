8b5ed0ac44d5407018fc1d905518115ffd318ec3
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-24 21:32:42 +0300

    Backend :  Updating the signup function in the user controller (to work with the new migrations)

--- WatchOut-Backend-Laravel/app/Http/Controllers/Api/V1/Auth/JWTAuthController.php
+++ WatchOut-Backend-Laravel/app/Http/Controllers/Api/V1/Auth/JWTAuthController.php
@@ -12,7 +12,7 @@ use Validator;
 use Auth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Symfony\Component\HttpFoundation\Response;
- 
+
 
 class JWTAuthController extends Controller
 {
@@ -32,36 +32,33 @@ class JWTAuthController extends Controller
     public function signup(Request $request)
     {
 
-        
+
         $validator = Validator::make($request->all(), [
             'firstname' => 'required|string|min:2|max:100',
             'lastname' => 'required|string|min:2|max:100',
+            'phonenumber' => 'required|number',
             'username' => 'required|string|max:100|unique:users',
             'password' => 'required|string|min:6',
-            'c_password' => 'required|same:password', 
-            'score' => 'required|string|',
-            'level' => 'required|string|',
-            // 'is_admin' => 'required|string|confirmed|min:6',
+            'c_password' => 'required|same:password',
         ]);
 
         if($validator->fails()) {
             return response()->json(["message" => "Validator Failed ! Check your submiited values again!"]);
         }
 
-     
+
         $user = new User;
 
         $user->firstname = $request->firstname;
         $user->lastname = $request->lastname;
         $user->username = $request->username;
         $user->password = bcrypt( $request->password);
-        $user->score = $request->score;
-        $user->level = $request->level;
+        $user->balance = 0;
         $user->is_admin = 0;
 
         $user->save();
 
-        $input = $request->only('username', 'password');  
+        $input = $request->only('username', 'password');
         $jwt_token = JWTAuth::attempt($input);
 
 
@@ -75,7 +72,7 @@ class JWTAuthController extends Controller
     {
         $input = $request->only('username', 'password');
         $jwt_token = null;
-  
+
         if (!$jwt_token = JWTAuth::attempt($input)) {
             return response()->json([
                 'success' => false,
@@ -92,15 +89,15 @@ class JWTAuthController extends Controller
 
     public function logout() {
         Auth::guard('api')->logout();
-    
+
         return response()->json([
             'status' => 'success',
             'message' => 'logout'
         ], 200);
-    }    
-    
-   
-    
+    }
+
+
+
     public function refresh(){
         return $this->respondWithToken(auth()->refresh());
     }
@@ -113,7 +110,7 @@ class JWTAuthController extends Controller
         ]);
     }
 
-    
-  
-  
+
+
+
 }
