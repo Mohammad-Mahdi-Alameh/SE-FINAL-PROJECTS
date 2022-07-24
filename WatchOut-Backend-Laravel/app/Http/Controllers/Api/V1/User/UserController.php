98800333416e00673f08774eb0c7b884887c9bf4
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-24 23:51:27 +0300

    Backend :  Adding and activating "api" middleware on every method in the user controller ;so requiring authentication (covered by jwt) to call any of them

--- WatchOut-Backend-Laravel/app/Http/Controllers/Api/V1/User/UserController.php
+++ WatchOut-Backend-Laravel/app/Http/Controllers/Api/V1/User/UserController.php
@@ -10,6 +10,10 @@ use Illuminate\Http\Request;
 use Validator;
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => []]);
+    }
 
     public function report(Request $request)
     {
