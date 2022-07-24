f6b1f6482cc57bfe72e4da453f9c4eb53b35f445
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-25 00:05:09 +0300

    Backend :  Creating AdminController

---
+++ WatchOut-Backend-Laravel/app/Http/Controllers/Api/V1/Admin/AdminController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Admin;
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Controllers\Controller;
+use App\Models\InfrastructuralProblem;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+  
+}
