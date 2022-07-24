ab283000abd7239940d8f612ec1b50c4a4ce3e9e
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-24 18:43:45 +0300

    Frontend :  Creating "getDefaultHeaders" private function in"ApiMainHeadersProvider" class that will initialize default headers for every api (whether it requires authentication or no)

--- WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/api/ApiMainHeadersProvider.kt
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/api/ApiMainHeadersProvider.kt
@@ -3,6 +3,12 @@ package com.example.watchout_frontend_kotlin.api
 class ApiMainHeadersProvider {
 
     companion object {
+        private const val CONTENT_TYPE = "Content-Type"
+
+        //Default headers used on all calls.
+        private fun getDefaultHeaders() = mapOf(
+            CONTENT_TYPE to "application/json",
+        )
 
     }
 }
