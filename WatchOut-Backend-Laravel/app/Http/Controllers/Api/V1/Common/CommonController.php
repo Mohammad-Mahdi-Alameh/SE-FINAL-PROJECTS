a8483aef89b0390a23e7d1509d8791fd372d176d
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-25 00:31:22 +0300

    Backend :  Creating CommonController and put in it common methods between user and admin to avoid redundancy

---
+++ WatchOut-Backend-Laravel/app/Http/Controllers/Api/V1/Common/CommonController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Common;
+use Symfony\Component\HttpFoundation\Response;
+use App\Models\InfrastructuralProblem;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Validator;
+class CommonController extends Controller
+{
+
+}
