aeb84e7c2295878fe1acda25b23e92cd1f007de6
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-24 18:49:17 +0300

    Frontend :  Creating "getAuthenticatedHeaders" public function in"ApiMainHeadersProvider" class that will set and provide default headers for any api (that requires authentication ) and will set and provide the authenticating headers (including basically JWT)

--- WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/api/ApiMainHeadersProvider.kt
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/api/ApiMainHeadersProvider.kt
@@ -4,11 +4,23 @@ class ApiMainHeadersProvider {
 
     companion object {
         private const val CONTENT_TYPE = "Content-Type"
+        private const val AUTHORIZATION = "Authorization"
+        private const val HEADER_ACCEPT = "Accept"
+
+        private fun getBearer(jwtToken: String) = "Bearer $jwtToken"
 
         //Public headers for calls that do not need an authenticated user.
         fun getPublicHeaders(): PublicHeaders =
             PublicHeaders().apply {
                 putAll(getDefaultHeaders())
+            }
+
+        //Returns both the default headers and the headers that are mandatory for authenticated users.
+        fun getAuthenticatedHeaders(jwtToken: String): AuthenticatedHeaders =
+            AuthenticatedHeaders().apply {
+                putAll(getDefaultHeaders())
+                put(HEADER_ACCEPT, "application/json")
+                put(AUTHORIZATION, getBearer(jwtToken))
             }
 
         //Default headers used on all calls.
