ca18263a1de11d1743390e2d440840f152f17aa3
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-24 07:41:01 +0300

    Frontend :  Implementing functions for encrypting the password of the logged in user and saving it in shared preferences

--- WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/others/Functions.kt
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/others/Functions.kt
@@ -1,13 +1,22 @@
 package com.example.watchout_frontend_kotlin.others
 
 import android.content.Context
+import android.preference.PreferenceManager
 import android.view.Gravity
 import android.view.View
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AlertDialog
 import com.example.watchout_frontend_kotlin.R
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
 
-    fun popupAlertDialog(context:Context,id :Int){
+fun popupAlertDialog(context:Context,id :Int){
         val view = View.inflate(context, id,null)
         val builder = AlertDialog.Builder(context)
         builder.setView(view)
@@ -24,3 +33,89 @@ import com.example.watchout_frontend_kotlin.R
         }
         popupDialog.show()
     }
+fun encryptAndSavePassword(context:Context, strToEncrypt: String): ByteArray {
+    val plainText = strToEncrypt.toByteArray(Charsets.UTF_8)
+    val keygen = KeyGenerator.getInstance("AES")
+    keygen.init(256)
+    val key = keygen.generateKey()
+    saveSecretKey(context, key)
+    val cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING")
+    cipher.init(Cipher.ENCRYPT_MODE, key)
+    val cipherText = cipher.doFinal(plainText)
+    saveInitializationVector(context, cipher.iv)
+
+    val baos = ByteArrayOutputStream()
+    val oos = ObjectOutputStream(baos)
+    oos.writeObject(cipherText)
+    val strToSave = String(android.util.Base64.encode(baos.toByteArray(), android.util.Base64.DEFAULT))
+    val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
+    val editor = sharedPref.edit()
+    editor.putString("encrypted_password", strToSave)
+    editor.apply()
+
+    return cipherText
+}
+
+fun getDecryptedPassword(context: Context): String {
+    val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
+    val strEncryptedPassword = sharedPref.getString("encrypted_password", "")
+    val bytes = android.util.Base64.decode(strEncryptedPassword, android.util.Base64.DEFAULT)
+    val ois = ObjectInputStream(ByteArrayInputStream(bytes))
+    val passwordToDecryptByteArray = ois.readObject() as ByteArray
+    val decryptedPasswordByteArray = decrypt(context, passwordToDecryptByteArray)
+
+    val decryptedPassword = StringBuilder()
+    for (b in decryptedPasswordByteArray) {
+        decryptedPassword.append(b.toChar())
+    }
+
+    return decryptedPassword.toString()
+}
+
+ fun decrypt(context:Context, dataToDecrypt: ByteArray): ByteArray {
+    val cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING")
+    val ivSpec = IvParameterSpec(getSavedInitializationVector(context))
+    cipher.init(Cipher.DECRYPT_MODE, getSavedSecretKey(context), ivSpec)
+    val cipherText = cipher.doFinal(dataToDecrypt)
+    return cipherText
+}
+
+ fun saveSecretKey(context:Context, secretKey: SecretKey) {
+    val baos = ByteArrayOutputStream()
+    val oos = ObjectOutputStream(baos)
+    oos.writeObject(secretKey)
+    val strToSave = String(android.util.Base64.encode(baos.toByteArray(), android.util.Base64.DEFAULT))
+    val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
+    val editor = sharedPref.edit()
+    editor.putString("secret_key", strToSave)
+    editor.apply()
+}
+
+ fun getSavedSecretKey(context: Context): SecretKey {
+    val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
+    val strSecretKey = sharedPref.getString("secret_key", "")
+    val bytes = android.util.Base64.decode(strSecretKey, android.util.Base64.DEFAULT)
+    val ois = ObjectInputStream(ByteArrayInputStream(bytes))
+    val secretKey = ois.readObject() as SecretKey
+    return secretKey
+}
+
+ fun saveInitializationVector(context: Context, initializationVector: ByteArray) {
+    val baos = ByteArrayOutputStream()
+    val oos = ObjectOutputStream(baos)
+    oos.writeObject(initializationVector)
+    val strToSave = String(android.util.Base64.encode(baos.toByteArray(), android.util.Base64.DEFAULT))
+    val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
+    val editor = sharedPref.edit()
+    editor.putString("initialization_vector", strToSave)
+    editor.apply()
+}
+
+ fun getSavedInitializationVector(context: Context) : ByteArray {
+    val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
+    val strInitializationVector = sharedPref.getString("initialization_vector", "")
+    val bytes = android.util.Base64.decode(strInitializationVector, android.util.Base64.DEFAULT)
+    val ois = ObjectInputStream(ByteArrayInputStream(bytes))
+    val initializationVector = ois.readObject() as ByteArray
+    return initializationVector
+}
