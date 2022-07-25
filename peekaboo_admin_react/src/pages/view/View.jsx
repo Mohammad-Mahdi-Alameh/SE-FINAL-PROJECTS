4785ab73d49d11b80c35318a349ea9c846c42850
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-26 00:56:34 +0300

    Frontend : Making a fetch call (get) to get the user information to view them on the screen and calling it in the useEffect

--- peekaboo_admin_react/src/pages/view/View.jsx
+++ peekaboo_admin_react/src/pages/view/View.jsx
@@ -2,6 +2,7 @@ import "./view.scss"
 import { useState, useEffect } from "react";
 
 const View = () => {
+    const base_url = "http://192.168.0.100:8000/api/v1/admin";
     let [info, setInfo] = useState([]);
     let urlString = window.location.href;
     let paramString = urlString.split('?')[1];
@@ -9,7 +10,22 @@ const View = () => {
     for (var pair of queryString.entries()) {
         var user_id = pair[1]
     }
+    const fetchInfo = async () => {
 
+            const res = await fetch(base_url +"/users?id=" + user_id);
+            const data = await res.json();
+            return data;
+
+    };
+
+    useEffect(() => {
+        const getInfo = async () => {
+            const serverInfo = await fetchInfo();
+            setInfo(serverInfo[0]);
+        };
+        getInfo();
+
+    }, []);
 
 
     
