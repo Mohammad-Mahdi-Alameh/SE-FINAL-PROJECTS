56a0ddf69ba19aec58b204fc02615ce274e02f71
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-25 20:39:18 +0300

    Frontend : Viewing the users in the datagrid and returning it

--- peekaboo_admin_react/src/components/table/Table.jsx
+++ peekaboo_admin_react/src/components/table/Table.jsx
@@ -1,11 +1,15 @@
 import "./table.scss";
 import { useState, useEffect } from "react";
+import{Link} from  "react-router-dom";
 import { DataGrid } from "@mui/x-data-grid";
+import { userColumns } from "../../datatablesource";
 
 
 const Table = () => {
   const [users, setUsers] = useState([]);
   const base_url = "http://192.168.0.100:8000/api/v1/admin";
+
+ 
 
   const getUsers = async () => {
     const res = await fetch(base_url + "/users");
@@ -22,7 +26,14 @@ const Table = () => {
   }, []);
   return (
     <div className="table">
-    Users
+    <DataGrid
+      className="datagrid"
+      rows={users}
+      columns={userColumns}
+      pageSize={9}
+      rowsPerPageOptions={[9]}
+      checkboxSelection
+    />
   </div>
   );
 };
