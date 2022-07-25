3f1c810ba41bb6c3d726eb86ac51aba03c785f80
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-25 23:58:47 +0300

    Frontend : Navigating to view page when view on specific user from the table is clicked

--- peekaboo_admin_react/src/components/table/Table.jsx
+++ peekaboo_admin_react/src/components/table/Table.jsx
@@ -1,23 +1,24 @@
 import "./table.scss";
 import { useState, useEffect } from "react";
-import{ useLocation} from  "react-router-dom";
+import { useNavigate , Link } from "react-router-dom";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns } from "../../datatablesource";
 
 
 const Table = () => {
+  let navigate=useNavigate();
   const [users, setUsers] = useState([]);
   const base_url = "http://192.168.0.100:8000/api/v1/admin";
 
 
 
-  const handleDelete = async(id) => {
-    const res = await fetch(base_url + "/delete_user/"+id, {
+  const handleDelete = async (id) => {
+    const res = await fetch(base_url + "/delete_user/" + id, {
       method: "PUT",
     });
     const result = await res.json();
-    if (result.message == "Deleted successfully"){
-    setUsers(users.filter((item) => item.id !== id));
+    if (result.message == "Deleted successfully") {
+      setUsers(users.filter((item) => item.id !== id));
     }
 
   };
@@ -30,7 +31,8 @@ const Table = () => {
       renderCell: (params) => {
         return (
           <div className="cellAction">
-              <div className="viewButton">View</div>
+            <div className="viewButton"
+              onClick={() =>navigate('/users/'+params.row.id)}>View</div>
             <div
               className="deleteButton"
               onClick={() => handleDelete(params.row.id)}
@@ -42,7 +44,7 @@ const Table = () => {
       },
     },
   ];
- 
+
 
   const getUsers = async () => {
     const res = await fetch(base_url + "/users");
@@ -50,7 +52,7 @@ const Table = () => {
     return data;
   };
 
-  
+
 
 
   useEffect(() => {
@@ -62,21 +64,21 @@ const Table = () => {
   }, []);
   return (
     <div className="table">
-    <div className="tableTitle">
-      Add New User
-      <Link to="/users/add" className="link">
-        Add New
-      </Link>
+      <div className="tableTitle">
+        Add New User
+        <Link to="/users/add" className="link">
+          Add New
+        </Link>
+      </div>
+      <DataGrid
+        className="datagrid"
+        rows={users}
+        columns={userColumns.concat(actionColumn)}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
     </div>
-    <DataGrid
-      className="datagrid"
-      rows={users}
-      columns={userColumns.concat(actionColumn)}
-      pageSize={5}
-      rowsPerPageOptions={[5]}
-      checkboxSelection
-    />
-  </div>
   );
 };
 
