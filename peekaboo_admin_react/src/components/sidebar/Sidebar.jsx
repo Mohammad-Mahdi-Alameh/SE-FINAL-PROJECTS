f3a8690115d6b41780985c8c6d636986bfb8cdab
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-25 17:20:50 +0300

    Frontend : Adding icons on the sidebar

--- peekaboo_admin_react/src/components/sidebar/Sidebar.jsx
+++ peekaboo_admin_react/src/components/sidebar/Sidebar.jsx
@@ -1,34 +1,44 @@
 import "./sidebar.scss";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import MyLocationIcon from '@mui/icons-material/MyLocation';
+import MapIcon from '@mui/icons-material/Map';
+import { Link } from "react-router-dom";
 const Sidebar = () => {
-    return (
-        <div className="sidebar">
-            <div className="top">
-                <span className="logo">PEEKABOO</span>
-            </div>
-            <hr />
-            <div className="center">
-                <ul>
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">PEEKABOO</span>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
 
-                    <p className="title">MANAGE</p>
-                    <li>
-                        <span>Users</span>
-                    </li>
+          <p className="title">MANAGE</p>
+          <li>
+            <PersonOutlineIcon className="icon" />
+            <span>Users</span>
+          </li>
 
-                    <li>
-                        <span>MAP</span>
-                    </li>
-                    
-                    <p className="title">USER</p>
-                    <li>
-                        <span>Profile</span>
-                    </li>
-                    <li>
-                        <span>Logout</span>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
+          <li>
+            <MapIcon className="icon" />
+            <span>MAP</span>
+          </li>
+
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <ExitToAppIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
