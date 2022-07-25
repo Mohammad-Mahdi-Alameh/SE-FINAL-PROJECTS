b0085eee1082b1c2da297a658091c9df58f71c79
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-25 17:24:06 +0300

    Frontend:Creating basic html for the navbar

---
+++ peekaboo_admin_react/src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./navbar.scss";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+const Navbar = () => {
+
+  return (
+    <div className="navbar">
+        </div>
+  );
+};
+
+export default Navbar;
