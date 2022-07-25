dd639e14d80533c57542ba634ace06a837b82f4f
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-26 00:53:21 +0300

    Frontend : Getting the user id in the url (sent from the users page )

--- peekaboo_admin_react/src/pages/view/View.jsx
+++ peekaboo_admin_react/src/pages/view/View.jsx
@@ -1,10 +1,19 @@
 import "./view.scss"
+import { useState, useEffect } from "react";
+
 const View = () => {
+    let [info, setInfo] = useState([]);
+    let urlString = window.location.href;
+    let paramString = urlString.split('?')[1];
+    let queryString = new URLSearchParams(paramString);
+    for (var pair of queryString.entries()) {
+        var user_id = pair[1]
+    }
 
 
 
-    return (<div className="view">View</div>)
-
+    
+ return (<div className="view">View</div>)
 
 }
 
