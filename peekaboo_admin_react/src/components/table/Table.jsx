429a70eee9377f29095da51d0a5773b06de7e63d
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-25 20:23:28 +0300

    Frontend : Starting  a get request to fetch all users from database (using fetch) when the Table loads (using useEffect)

--- peekaboo_admin_react/src/components/table/Table.jsx
+++ peekaboo_admin_react/src/components/table/Table.jsx
@@ -1,6 +1,23 @@
 import "./table.scss";
+import { useState, useEffect } from "react";
 
 const Table = () => {
+  const [users, setUsers] = useState([]);
+  const base_url = "http://192.168.0.100:8000/api/v1/admin";
+
+  const getUsers = async () => {
+    const res = await fetch(base_url + "/users");
+    const data = await res.json();
+    return data;
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      const usersFromServer = await getUsers();
+      setUsers(usersFromServer);
+    };
+    getData();
+  }, []);
   return (
     <div className="table">
   
