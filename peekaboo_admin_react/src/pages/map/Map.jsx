62ea4b84e16aaa54f86cbb433e003a52f3219cbf
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-26 11:09:22 +0300

    Frontend : Setting up the the main google map settings and show it on the screen (in the map component)

--- peekaboo_admin_react/src/pages/map/Map.jsx
+++ peekaboo_admin_react/src/pages/map/Map.jsx
@@ -1,9 +1,31 @@
+import { GoogleMap,useLoadScript } from "@react-google-maps/api"
 import "./map.scss"
+const libraries = ["places"]
+const mapContainerStyle = {
+    width : "80vw",
+    height : "80vh"
+}
+const center ={
+    lat:43.653225,
+    lng:-79.383186,
+}
 const Map = () => {
 
+    const {isLoaded,loadError } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        libraries
+    });
+
+    if(loadError) return "Error loading maps";
+    if(!isLoaded) return "Loading maps";
 
 
-    return (<div className="map">Map</div>)
+    return (<div className="map">
+        <GoogleMap 
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={center}
+        ></GoogleMap></div>)
 
 
 }
