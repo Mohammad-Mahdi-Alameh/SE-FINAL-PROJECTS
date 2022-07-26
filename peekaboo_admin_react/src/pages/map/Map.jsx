cba60f91ec7fac393b14bfeba3a657f225891e60
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-26 12:32:06 +0300

    Frontend : Refactoring the way we are handling the onMapClick by by  using the useCallback (and moving the onClick logic to it) so we can avoid recreating the function on every render

--- peekaboo_admin_react/src/pages/map/Map.jsx
+++ peekaboo_admin_react/src/pages/map/Map.jsx
@@ -1,7 +1,7 @@
 import { GoogleMap,Marker,MarkerClusterer,useLoadScript } from "@react-google-maps/api"
 import "./map.scss"
 import mapStyles from "../../mapStyles"
-import { useState  } from "react"
+import React ,{ useState , useCallback } from "react"
 const libraries = ["places"]
 const mapContainerStyle = {
     width : "80vw",
@@ -16,7 +16,16 @@ const options ={
 }
 const Map = () => {
     const [markers , setMarkers]=useState([])
-   
+
+    const onMapClick = React.useCallback((event)=>
+    setMarkers((current) => [
+        ...current,
+        {
+            lat : event.latLng.lat(),
+            lng : event.latLng.lng(),
+            time : new Date()
+        }
+    ]))
 
     const {isLoaded,loadError } = useLoadScript({
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
@@ -33,15 +42,7 @@ const Map = () => {
         zoom={8}
         center={center}
         options={options}
-        onClick={(event)=>
-            setMarkers((current) => [
-                ...current,
-                {
-                    lat : event.latLng.lat(),
-                    lng : event.latLng.lng(),
-                    time : new Date()
-                }
-            ])}
+        onClick={onMapClick}
         >
             {markers.map((marker) => (
                 <Marker
