5046388d3269751e07725952bf61251994b5a6bd
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-27 04:16:19 +0300

    Frontend :  Creating "getAllInfras" get method in the LaravelRestApi interface with authenticated headers (and reformating the file)

--- WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/api/LaravelRestApi.kt
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/api/LaravelRestApi.kt
@@ -2,29 +2,40 @@ package com.example.watchout_frontend_kotlin.api
 
 import com.example.watchout_frontend_kotlin.models.*
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.HeaderMap
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface LaravelRestApi {
 
     @POST("/api/v1/user/login")
-    fun logIn(@HeaderMap authedHeaders: PublicHeaders,@Body loginInfo: LoginInfo): Call<LoginResponse>
+    fun logIn(
+        @HeaderMap authedHeaders: PublicHeaders,
+        @Body loginInfo: LoginInfo
+    ): Call<LoginResponse>
 
     @POST("/api/v1/user/signup")
-    fun signUp(@HeaderMap authedHeaders: PublicHeaders,@Body signupInfo: SignupInfo): Call<SignupResponse>
+    fun signUp(
+        @HeaderMap authedHeaders: PublicHeaders,
+        @Body signupInfo: SignupInfo
+    ): Call<SignupResponse>
 
     @POST("/api/v1/user/report")
-    fun report(@HeaderMap authedHeaders: AuthenticatedHeaders,@Body reportInfo: ReportInfo): Call<ReportResponse>
+    fun report(
+        @HeaderMap authedHeaders: AuthenticatedHeaders,
+        @Body reportInfo: ReportInfo
+    ): Call<ReportResponse>
 
     //infras : infrastructural problems
     @POST("/api/v1/user/getNearInfras")
-    fun getNearInfras(@HeaderMap authedHeaders: AuthenticatedHeaders,@Body getNearInfrasInfo: GetNearInfrasInfo): Call<List<GetInfrasResponse>>
+    fun getNearInfras(
+        @HeaderMap authedHeaders: AuthenticatedHeaders,
+        @Body getNearInfrasInfo: GetNearInfrasInfo
+    ): Call<List<GetInfrasResponse>>
 
-
-
-
-
+    @GET("/api/v1/user/getAllInfras/")
+    fun getAllInfras(
+        @HeaderMap authedHeaders: AuthenticatedHeaders,
+        @Query("user_id") user_id : String?,
+    ): Call<List<GetInfrasResponse>>
 
 
 
