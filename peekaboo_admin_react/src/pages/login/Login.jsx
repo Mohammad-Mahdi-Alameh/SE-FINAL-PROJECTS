d9211b8f404c7e88ebbbf3d4494bcdb52d2203f3
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-28 14:18:00 +0300

    Frontend : Creating basic html for login page (and  creating 2 use state one for username and on efor password)

--- peekaboo_admin_react/src/pages/login/Login.jsx
+++ peekaboo_admin_react/src/pages/login/Login.jsx
@@ -1,9 +1,41 @@
 import "./login.scss"
+import { useState } from 'react';
+
+import logo from "../../PEEKABOO-white.svg"
+
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    return (<div className="login"><div className="welcome">Welcome to<img src={logo} alt="" />
+    </div>
+        <div className="please">Please login to continue :</div>
+        <div className="form-layout">
+            <div className="form">
+                <div className="input-layout">
+
+                    <label>Username  </label>
+                    <input
+                        type="text"
+                        placeholder={"Enter Your Username"}
+                        value={username}
+                    />
+                </div>
+                <div className="input-layout">
+                    <label>Password </label>
+                    <input
+                        type="password"
+                        placeholder={"Enter Your Password"}
+                        value={password}
+                    />
+                </div>
+            </div>
+        </div>
 
 
 
-    return (<div className="login">Login</div>)
+    </div>)
 
 
 }
