58291ad25283eecc9a5073941ce8e1b7e1453b50
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-29 22:57:50 +0300

    Frontend : Creating function addMarker that takes latitude, longitude and type and adding the marker on the map according to its type , and calling it in the set markers method and after successful report attempt (the logic of this function was implemented before , but now we've put it in function to avoid code redundancy)

--- WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/activities/MapsActivity.kt
+++ WatchOut-Frontend-Kotlin/app/src/main/java/com/example/watchout_frontend_kotlin/activities/MapsActivity.kt
@@ -48,8 +48,8 @@ import com.google.firebase.ktx.Firebase
 import com.google.gson.Gson
 import org.json.JSONArray
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private lateinit var drawerLayout: DrawerLayout
@@ -57,6 +57,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var hamburger: ImageView
     private lateinit var coins: TextView
     private lateinit var report: Button
+    private lateinit var latLng: LatLng
     private lateinit var fusedLocClient: FusedLocationProviderClient
     private var public = PublicFunctions()
     private var database: FirebaseDatabase = FirebaseDatabase.getInstance()
@@ -279,8 +280,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 Log.i("long", locationLong.toString())
                 Log.i("speed", speed.toString())
 
-
-
                 if (locationLat != null && locationLong != null && speed != null) {
                     // create a LatLng object from location
                     val latLng = LatLng(locationLat, locationLong)
@@ -293,101 +292,96 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     val update = CameraUpdateFactory.newLatLngZoom(latLng, 16.0f)
                     //update the camera with the CameraUpdate object
                     mMap.moveCamera(update)
-                    val nearInfras = getNearInfras(locationLat, locationLong)
-//                    val array = JSONArray(nearInfras)
-//                    (0 until array.length()).forEach {
-//                        var infra = array.getJSONObject(it)
-//                        val timeRemaining = getDistance(
-//                            locationLat,
-//                            locationLong,
-//                            infra["latitude"] as Double,
-//                            infra["longitude"] as Double
-//                        ) * 1000 / speed
-//                        if (timeRemaining < 7) {
-////                                sendNotification()
-//                            Log.i("hamdella", "hamdella")
-//                        }
+
+                    } else {
+                        Log.i("Error", "Error")
 
                     }
+
+                }
+        }
+
+        // show this toast if there is an error while reading from the database
+        override fun onCancelled(error: DatabaseError) {
+            Toast.makeText(applicationContext, "Could not read from database", Toast.LENGTH_LONG)
+                .show()
+        }
+
+
+    }
+
+    private fun bitmapFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
+        // below line is use to generate a drawable.
+        val vectorDrawable = ContextCompat.getDrawable(context, vectorResId)
+
+        // below line is use to set bounds to our vector drawable.
+        vectorDrawable!!.setBounds(
+            0,
+            0,
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight
+        )
+
+        // below line is use to create a bitmap for our
+        // drawable which we have added.
+        val bitmap = Bitmap.createBitmap(
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+
+        // below line is use to add bitmap in our canvas.
+        val canvas = Canvas(bitmap)
+
+        // below line is use to draw our
+        // vector drawable in canvas.
+        vectorDrawable.draw(canvas)
+
+        // after generating our bitmap we are returning our bitmap.
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+
+
+    private fun getAllInfras() {
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
+        val jwtToken = sharedPref.getString("token", "")
+//        val id =sharedPref.getString("user_id","")
+        val apiService = RestApiService()
+        val authenticatedHeaders =
+            jwtToken?.let { ApiMainHeadersProvider.getAuthenticatedHeaders(it) }
+        if (authenticatedHeaders != null) {
+            apiService.getAllInfras(authenticatedHeaders, 0) {
+                if (it?.size != null) {
+                    Log.i("All Infras", Gson().toJson(it))
+                    setMarkers(Gson().toJson(it))
                 } else {
                     Log.i("Error", "Error")
 
                 }
-
-}
-
-
-// show this toast if there is an error while reading from the database
-override fun onCancelled(error: DatabaseError) {
-    Toast.makeText(applicationContext, "Could not read from database", Toast.LENGTH_LONG)
-        .show()
-}
-
-
-}
-
-private fun bitmapFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
-    // below line is use to generate a drawable.
-    val vectorDrawable = ContextCompat.getDrawable(context, vectorResId)
-
-    // below line is use to set bounds to our vector drawable.
-    vectorDrawable!!.setBounds(
-        0,
-        0,
-        vectorDrawable.intrinsicWidth,
-        vectorDrawable.intrinsicHeight
-    )
-
-    // below line is use to create a bitmap for our
-    // drawable which we have added.
-    val bitmap = Bitmap.createBitmap(
-        vectorDrawable.intrinsicWidth,
-        vectorDrawable.intrinsicHeight,
-        Bitmap.Config.ARGB_8888
-    )
-
-    // below line is use to add bitmap in our canvas.
-    val canvas = Canvas(bitmap)
-
-    // below line is use to draw our
-    // vector drawable in canvas.
-    vectorDrawable.draw(canvas)
-
-    // after generating our bitmap we are returning our bitmap.
-    return BitmapDescriptorFactory.fromBitmap(bitmap)
-}
-
-
-private fun getAllInfras() {
-    val sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
-    val jwtToken = sharedPref.getString("token", "")
-//        val id =sharedPref.getString("user_id","")
-    val apiService = RestApiService()
-    val authenticatedHeaders =
-        jwtToken?.let { ApiMainHeadersProvider.getAuthenticatedHeaders(it) }
-    if (authenticatedHeaders != null) {
-        apiService.getAllInfras(authenticatedHeaders,0) {
-            if (it?.size != null) {
-                Log.i("All Infras", Gson().toJson(it))
-                setMarkers(Gson().toJson(it))
-            } else {
-                Log.i("Error", "Error")
-
             }
+        }
+
+    }
+
+    private fun setMarkers(data: String) {
+        val array = JSONArray(data)
+        (0 until array.length()).forEach {
+            var infra = array.getJSONObject(it)
+            addMarker(
+                infra["latitude"] as Double,
+                infra["longitude"] as Double,
+                infra["type"] as String
+            )
+
         }
     }
 
-}
+    private fun addMarker(latitude: Double, longitude: Double, type: String) {
 
-private fun setMarkers(data: String) {
-    val array = JSONArray(data)
-    (0 until array.length()).forEach {
-        var infra = array.getJSONObject(it)
-        var latLng = LatLng(infra["latitude"] as Double, infra["longitude"] as Double)
-        Log.i("lat", infra["latitude"].toString() + infra["longitude"].toString())
-        Log.i("lng", infra["longitude"].toString())
-        if (infra["type"] != null) {
-            when (infra["type"]) {
+        latLng = LatLng(latitude, longitude)
+
+        if (type != null) {
+            when (type) {
                 "hole" -> {
                     mMap.addMarker(
                         MarkerOptions().position(latLng)
@@ -397,7 +391,12 @@ private fun setMarkers(data: String) {
                 "blockage" -> {
                     mMap.addMarker(
                         MarkerOptions().position(latLng)
-                            .icon(bitmapFromVector(applicationContext, R.drawable.blockage_marker))
+                            .icon(
+                                bitmapFromVector(
+                                    applicationContext,
+                                    R.drawable.blockage_marker
+                                )
+                            )
                     )
                 }
                 "turn" -> {
@@ -417,6 +416,8 @@ private fun setMarkers(data: String) {
             }
 
         }
+
+
     }
 
 //        array?.let {
@@ -425,7 +426,6 @@ private fun setMarkers(data: String) {
 //                val latLng = LatLng(json["latitude"] as Double, json["longitude"] as Double)
 //            }
 //        }
-}
 //    private fun setMarkers(data : String){
 //        val latLng = LatLng(location.latitude, location.longitude)
 //        // create a marker at the exact location
@@ -433,119 +433,121 @@ private fun setMarkers(data: String) {
 //            .title("You are currently here!"))
 //    }
 
-private fun getInfraType(v: View): String {
-    val type = ""
-    if (v != null) {
-        when (v.id) {
-            R.id.hole -> {
-                return type.replace("", "hole")
-            }
-            R.id.turn -> {
-                return type.replace("", "turn")
-            }
-            R.id.bump -> {
-                return type.replace("", "bump")
-            }
-            R.id.blockage -> {
-                return type.replace("", "blockage")
+    private fun getInfraType(v: View): String {
+        val type = ""
+        if (v != null) {
+            when (v.id) {
+                R.id.hole -> {
+                    return type.replace("", "hole")
+                }
+                R.id.turn -> {
+                    return type.replace("", "turn")
+                }
+                R.id.bump -> {
+                    return type.replace("", "bump")
+                }
+                R.id.blockage -> {
+                    return type.replace("", "blockage")
+                }
             }
         }
+        return type
     }
-    return type
-}
 
-private fun report(type: String, latitude: Double, longitude: Double) {
-    val sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
-    val jwtToken = sharedPref.getString("token", "")
-    val apiService = RestApiService()
-    val authenticatedHeaders =
-        jwtToken?.let { ApiMainHeadersProvider.getAuthenticatedHeaders(it) }
-    if (authenticatedHeaders != null) {
-        val reportInfo = ReportInfo(
-            latitude = latitude,
-            longitude = longitude,
-            type = type,
-            id = sharedPref.getString("user_id", "")?.toInt()
-        )
+    private fun report(type: String, latitude: Double, longitude: Double) {
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
+        val jwtToken = sharedPref.getString("token", "")
+        val apiService = RestApiService()
         val authenticatedHeaders =
-            ApiMainHeadersProvider.getAuthenticatedHeaders("")
-        apiService.report(authenticatedHeaders, reportInfo) {
-            if (it?.message == "Infrastructural problem reported successfully") {
-                Log.i("Report Succeeded", it.message)
-            } else {
+            jwtToken?.let { ApiMainHeadersProvider.getAuthenticatedHeaders(it) }
+        if (authenticatedHeaders != null) {
+            val reportInfo = ReportInfo(
+                latitude = latitude,
+                longitude = longitude,
+                type = type,
+                id = sharedPref.getString("user_id", "")?.toInt()
+            )
+            val authenticatedHeaders =
+                ApiMainHeadersProvider.getAuthenticatedHeaders("")
+            apiService.report(authenticatedHeaders, reportInfo) {
+                if (it?.message == "Infrastructural problem reported successfully") {
+                    val latLng = LatLng(latitude, longitude)
+                    Log.i("Report Succeeded", it.message)
+                    addMarker(latitude,longitude,type)
+                } else {
 
-                Log.i("Error", "Report Failed !")
+                    Log.i("Error", "Report Failed !")
+                }
             }
         }
     }
-}
 
-private fun setupLocClient() {
-    fusedLocClient =
-        LocationServices.getFusedLocationProviderClient(this)
-}
-
-@SuppressLint("MissingPermission")
-private fun reportFunctionCaller(v: View) {
-    fusedLocClient.lastLocation.addOnCompleteListener {
-        // lastLocation is a task running in the background
-        val location = it.result //obtain location
-        //reference to the database
-        if (location != null) {
-            val type = getInfraType(v)
-            report(type, location.latitude, location.longitude)
-        } else {
-            // if location is null , log an error message
-            Log.e("error", "No location found")
-        }
-
-
+    private fun setupLocClient() {
+        fusedLocClient =
+            LocationServices.getFusedLocationProviderClient(this)
     }
-}
 
-private fun getDistance(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
-    val theta = lon1 - lon2
-    var dist = (Math.sin(deg2rad(lat1))
-            * Math.sin(deg2rad(lat2))
-            + (Math.cos(deg2rad(lat1))
-            * Math.cos(deg2rad(lat2))
-            * Math.cos(deg2rad(theta))))
-    dist = Math.acos(dist)
-    dist = rad2deg(dist)
-    dist *= 60 * 1.1515
-    dist *= 1.609344   //transform dist from miles to km
-    return dist
-}
-
-private fun deg2rad(deg: Double): Double {
-    return deg * Math.PI / 180.0
-}
-
-private fun rad2deg(rad: Double): Double {
-    return rad * 180.0 / Math.PI
-}
-
-private fun getNearInfras(latitude: Double, longitude: Double) {
-    val jwtToken = sharedPref.getString("token", "")
-    val apiService = RestApiService()
-    val authenticatedHeaders =
-        jwtToken?.let { ApiMainHeadersProvider.getAuthenticatedHeaders(it) }
-    if (authenticatedHeaders != null) {
-        val getNearInfrasInfo = GetNearInfrasInfo(
-            base_latitude = latitude,
-            base_longitude = longitude
-        )
-        apiService.getNearInfras(authenticatedHeaders, getNearInfrasInfo) {
-            if (it?.size != null) {
-                Log.i("Near Infras", Gson().toJson(it))
-                editor.putString("Near Infras", Gson().toJson(it))
+    @SuppressLint("MissingPermission")
+    private fun reportFunctionCaller(v: View) {
+        fusedLocClient.lastLocation.addOnCompleteListener {
+            // lastLocation is a task running in the background
+            val location = it.result //obtain location
+            //reference to the database
+            if (location != null) {
+                val type = getInfraType(v)
+                report(type, location.latitude, location.longitude)
             } else {
-                Log.i("Error", "Error")
-
+                // if location is null , log an error message
+                Log.e("error", "No location found")
             }
-        }
 
+
+        }
     }
-}
+
+    private fun getDistance(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
+        val theta = lon1 - lon2
+        var dist = (Math.sin(deg2rad(lat1))
+                * Math.sin(deg2rad(lat2))
+                + (Math.cos(deg2rad(lat1))
+                * Math.cos(deg2rad(lat2))
+                * Math.cos(deg2rad(theta))))
+        dist = Math.acos(dist)
+        dist = rad2deg(dist)
+        dist *= 60 * 1.1515
+        dist *= 1.609344   //transform dist from miles to km
+        return dist
+    }
+
+    private fun deg2rad(deg: Double): Double {
+        return deg * Math.PI / 180.0
+    }
+
+    private fun rad2deg(rad: Double): Double {
+        return rad * 180.0 / Math.PI
+    }
+
+    private fun getNearInfras(latitude: Double, longitude: Double) {
+        val jwtToken = sharedPref.getString("token", "")
+        val apiService = RestApiService()
+        val authenticatedHeaders =
+            jwtToken?.let { ApiMainHeadersProvider.getAuthenticatedHeaders(it) }
+        if (authenticatedHeaders != null) {
+            val getNearInfrasInfo = GetNearInfrasInfo(
+                base_latitude = latitude,
+                base_longitude = longitude
+            )
+            apiService.getNearInfras(authenticatedHeaders, getNearInfrasInfo) {
+                if (it?.size != null) {
+                    Log.i("Near Infras", Gson().toJson(it))
+                    editor.putString("Near Infras", Gson().toJson(it))
+                } else {
+                    Log.i("Error", "Error")
+
+                }
+            }
+
+        }
+    }
 }
 
