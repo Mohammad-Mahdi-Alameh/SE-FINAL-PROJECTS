c3f38d048d1d61fcb94c75c4270c45e44da01e96
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-08-01 14:16:16 +0300

    Frontend : Fetching the the false infras from db when the the Falsealarm component loads(in use effect) by axios

--- peekaboo_admin_react/src/pages/false alarms/FalseAlarms.jsx
+++ peekaboo_admin_react/src/pages/false alarms/FalseAlarms.jsx
@@ -1,6 +1,33 @@
 import "./false-alarms.scss"
+import { DataGrid } from "@mui/x-data-grid";
+import { falseInfrasColumns , base_url } from "../../Constants";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const FalseAlarms = () => {
+    const [falseInfras, setFalseInfras] = useState([]);
+  
+
+    const getFalseInfras = async () => {
+      const res = await fetch(base_url + "/false_infras");
+      const data = await res.json();
+      return data;
+    };
+  
+  
+  
+  
+    useEffect(() => {
+      const getData = async () => {
+        const falseInfrasFromServer = await getFalseInfras();
+        setFalseInfras(falseInfrasFromServer);
+      };
+      getData();
+    }, []);
+    return (<>
+      </>
+    );
 }
 
 export default FalseAlarms
