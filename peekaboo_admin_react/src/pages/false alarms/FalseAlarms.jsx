2c63f7dac3fbb143ff8e596d1ee55fbe8ac2435a
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-08-02 16:28:13 +0300

    Frontend : Enhancing in genral the  the FalseAlarm page (general formatting , adding comments , importing index.scss)

--- peekaboo_admin_react/src/pages/false alarms/FalseAlarms.jsx
+++ peekaboo_admin_react/src/pages/false alarms/FalseAlarms.jsx
@@ -1,64 +1,72 @@
-import "./false-alarms.scss"
-import { DataGrid } from "@mui/x-data-grid";
-import { falseInfrasColumns , base_url } from "../../Constants";
+//React Hooks
 import { useState, useEffect } from "react";
+//Style File
+import "../../index.scss"
+//Components
+import { DataGrid } from "@mui/x-data-grid";
+//Constants
+import { falseInfrasColumns, base_url } from "../../Constants";
+//Axios
 import axios from "axios";
 
-const FalseAlarms = () => {
-    const [falseInfras, setFalseInfras] = useState([]);
-  
-   
-  
-  
-    const handleAccept = async (id) => {
-        const res = await axios.put(base_url + "/delete_infra/" + id);
-        const result = await res.data;
-        if (result.message == "Deleted successfully") {
-          setFalseInfras(falseInfras.filter((item) => item.id !== id));
-        }
-    
-      };
-    const handleReject = async (id) => {
-        const res = await axios.put(base_url + "/reset_infra_status/" + id);
-        const result = await res.data;
-        if (result.message == "Reseted successfully") {
-          setFalseInfras(falseInfras.filter((item) => item.id !== id));
-        }
-    
-      };
+//Using Infras as a shortcut of Infrastructural problems//
 
-    const getFalseInfras = async () => {
-      const res = await axios.get(base_url + "/false_infras");
-      const data = await res.data;
-      return data;
+const FalseAlarms = () => {
+  //useState()
+  const [falseInfras, setFalseInfras] = useState([]);
+  ////
+
+  //useEffect()
+  useEffect(() => {
+    const getData = async () => {
+      const falseInfrasFromServer = await getFalseInfras();
+      setFalseInfras(falseInfrasFromServer);
     };
-  
-  
-  
-  
-    useEffect(() => {
-      const getData = async () => {
-        const falseInfrasFromServer = await getFalseInfras();
-        setFalseInfras(falseInfrasFromServer);
-      };
-      getData();
-    }, []);
-    return (<>
-      {falseInfras.length>0 && <div className="table">
-        <div className="tableTitle">
+    getData();
+  }, []);
+  ////
+
+  //functions and handlers
+  const getFalseInfras = async () => {
+    const res = await axios.get(base_url + "/false_infras");
+    const data = await res.data;
+    return data;
+  };
+  const handleAccept = async (id) => {
+    try {
+      const res = await axios.put(base_url + "/fix_infra/" + id);
+      setFalseInfras(falseInfras.filter((item) => item.id !== id));
+    } catch (error) {
+      console.log(error)
+    };
+  }
+  const handleReject = async (id) => {
+    try {
+      const res = await axios.put(base_url + "/reject_false_infra/" + id);
+      setFalseInfras(falseInfras.filter((item) => item.id !== id));
+    } catch (error) {
+      console.log(error)
+    };
+
+  };
+  ////
+
+  return (<>
+    {falseInfras.length>0 && <div className="table">
+      <div className="tableTitle">
          ////
-        </div>
-        <DataGrid
-          className="datagrid"
-          rows={falseInfras}
-          columns={falseInfrasColumns}
-          pageSize={5}
-          rowsPerPageOptions={[5]}
-          checkboxSelection
-        />
-      </div>}
-      </>
-    );
+      </div>
+      <DataGrid
+        className="datagrid"
+        rows={falseInfras}
+        columns={falseInfrasColumns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>}
+  </>
+  );
 }
 
 export default FalseAlarms;
