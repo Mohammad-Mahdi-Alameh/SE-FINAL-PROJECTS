45c32024de083cbf9d172f2c6db2253a35c8597e
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-08-02 11:44:23 +0300

    Frontend : Reformating the file (Map.jsx) , adding some comments, and make it more organized

--- peekaboo_admin_react/src/pages/map/Map.jsx
+++ peekaboo_admin_react/src/pages/map/Map.jsx
@@ -1,100 +1,39 @@
-import { GoogleMap, InfoWindow, Marker, MarkerClusterer, useLoadScript } from "@react-google-maps/api"
-import "../../index.scss"
-import SearchBar from "../../components/SearchBar";
-import compass from "../../compass.svg"
-import mapStyles from "./mapStyles"
+//React Hooks
 import React, { useState, useCallback, useRef, useEffect } from "react"
-import { formatRelative } from "date-fns";
+//Style File
+import "../../index.scss"
+//Google Maps
+import { GoogleMap, InfoWindow, Marker, MarkerClusterer, useLoadScript } from "@react-google-maps/api"
+//Components
+import SearchBar from "../../components/SearchBar";
+//Constants
 import { base_url } from "../../Constants";
-import "@reach/combobox/styles.css";
+import { libraries, mapContainerStyle, options } from "./mapConstants";
+//Axios
 import axios from "axios";
-import { libraries,mapContainerStyle,options } from "./mapConstants";
+//Svg's
+import compass from "../../compass.svg"
 
-////
-
+//Infras : Infrastructural problems
 
 const Map = () => {
+    //useState()
     const [markers, setMarkers] = useState([])
     const [infras, setInfras] = useState([])
     const [selected, setSelected] = useState(null)
     const [clicked, setClicked] = useState(null)
+    ////
+
+    //useRef()
     const mapRef = React.useRef();
+    ////
+
+    //useCallback()
     const onMapLoad = React.useCallback((map) => {
         mapRef.current = map;
         getLiveLocation();
     });
 
-    //Infras : Infrastructural problems
-    const getAllInfras = async () => {
-        const res = await axios.get(base_url + "/get_all_infras/0");
-        const data = await res.data;
-        return data;
-    };
-   
-    useEffect(() => {
-        const getData = async () => {
-            const infrasFromServer = await getAllInfras();
-            setInfras(infrasFromServer);
-            ///////
-        };
-        getData();
-    }, []);
-
-    useEffect(() => {
-
-        // console.log(infras)
-        infras.map(infra => {
-            setMarkers((current) => [
-                ...current,
-                {
-                    id: infra.id,
-                    lat: infra.latitude,
-                    lng: infra.longitude,
-                    type: infra.type,
-                    date: infra.created_at,
-                    by: infra.user_id
-
-                    /////
-                    // time: new Date()
-                }
-            ])
-        })
-
-
-    }, [infras]);
-
-    const handleDelete = async (id) => {
-        try{  const res = await axios.put(base_url + "/fix_infra/" + id);
-            setInfras(infras.filter((item) => item.id !== id));
-            setMarkers(markers.filter((item) => item.id !== id));
-            setSelected(null);
-          }
-          catch(err){
-            console.log(err)
-          }
-      
-        };
-
-
-    function getLiveLocation() {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                panTo({
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                });
-            },
-            () => null
-        );
-    }
-
-    function handleDrag(marker){
-            // console.log(marker.lat)
-    
-    ondragend = (event) =>{
-        console.log(event)
-    }
-    }
     const panTo = React.useCallback(({ lat, lng }) => {
         mapRef.current.panTo({ lat, lng });
         mapRef.current.setZoom(14);
@@ -110,15 +49,67 @@ const Map = () => {
         //     }
         // ])
         setClicked(event)
+    );
+    ////
+
+    //useEffect()
+    useEffect(() => {
+        const getData = async () => {
+            const infrasFromServer = await getAllInfras();
+            setInfras(infrasFromServer);
+        };
+        getData();
+    }, []);
+
+    useEffect(() => {
+        infras.map(infra => {
+            setMarkers((current) => [
+                ...current,
+                {
+                    id: infra.id,
+                    lat: infra.latitude,
+                    lng: infra.longitude,
+                    type: infra.type,
+                    date: infra.created_at,
+                    by: infra.user_id
+                    // time: new Date()
+                }
+            ])
+        })
+    }, [infras]);
+    ////
+
+    //functions and handlers
+    const getAllInfras = async () => {
+        const res = await axios.get(base_url + "/get_all_infras/0");
+        const data = await res.data;
+        return data;
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            const res = await axios.put(base_url + "/fix_infra/" + id);
+            setInfras(infras.filter((item) => item.id !== id));
+            setMarkers(markers.filter((item) => item.id !== id));
+            setSelected(null);
+        }
+        catch (err) {
+            console.log(err)
+        }
+
+    };
+
+    function getLiveLocation() {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                panTo({
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                });
+            },
+            () => null
         );
-
-    const { isLoaded, loadError } = useLoadScript({
-        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-        libraries
-    });
-
-    if (loadError) return "Error loading maps";
-    if (!isLoaded) return "Loading maps";
+    }
 
     function Locate({ panTo }) {
         return (<button className="locate" onClick={() => {
@@ -128,7 +119,17 @@ const Map = () => {
             <img src={compass} alt="" /></button>
         );
     }
+    ////
 
+    //useLoadScript()
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        libraries
+    });
+
+    if (loadError) return "Error loading maps";
+    if (!isLoaded) return "Loading maps";
+    ////
 
     return (<div className="map">
 
@@ -140,15 +141,15 @@ const Map = () => {
             onLoad={onMapLoad}
         >
 
-            {markers.map((marker,index) => (
+            {markers.map((marker, index) => (
                 <Marker
                     key={index}
                     position={{ lat: marker.lat, lng: marker.lng }}
                     onClick={() => {
                         setSelected(marker);
                         setClicked(null)
-                    }} 
-                  />
+                    }}
+                />
 
             ))}
             {
@@ -157,7 +158,7 @@ const Map = () => {
                         <p> {selected.type}</p>
 
                         <p> {selected.by}</p>
-                        <button onClick={()=>handleDelete(selected.id)}>Delete</button>
+                        <button onClick={() => handleDelete(selected.id)}>Delete</button>
                     </div>
 
                 </InfoWindow>
@@ -165,25 +166,21 @@ const Map = () => {
                 ) : null}
             {
                 clicked ? (<InfoWindow position={{ lat: clicked.latLng.lat(), lng: clicked.latLng.lng() }} onCloseClick={() => { setClicked(null) }}>
-                 <div>
-                            <input type={"radio"} name={"infra_type"} value={"Hole"}/>
-                            <label for={"Hole"}>Hole</label><br/>
-                            <input type={"radio"} name={"infra_type"} value={"Bump"}/>
-                            <label for={"Bump"}>Bump</label><br/>
-                            <input type={"radio"} name={"infra_type"} value={"Turn"}/>
-                            <label for={"Turn"}>Turn</label><br/>
-                            <input type={"radio"} name={"infra_type"} value={"Blockage"}/>
-                            <label for={"Blockage"}>Blockage</label><br/>
-                            <input type={"submit"} value="Add"  />
+                    <div>
+                        <input type={"radio"} name={"infra_type"} value={"Hole"} />
+                        <label for={"Hole"}>Hole</label><br />
+                        <input type={"radio"} name={"infra_type"} value={"Bump"} />
+                        <label for={"Bump"}>Bump</label><br />
+                        <input type={"radio"} name={"infra_type"} value={"Turn"} />
+                        <label for={"Turn"}>Turn</label><br />
+                        <input type={"radio"} name={"infra_type"} value={"Blockage"} />
+                        <label for={"Blockage"}>Blockage</label><br />
+                        <input type={"submit"} value="Add" />
                     </div>
 
                 </InfoWindow>
 
                 ) : null}
-
-
-
-
 
         </GoogleMap>
         <SearchBar panTo={panTo} />
