ad262d9a9599b5e34ea8c7a9f2fcb4b6ba1a7d6e
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-08-05 02:36:10 +0300

    Frontend : Adding comments to the Sidebar component and formatting the file

--- peekaboo_admin_react/src/components/sidebar/Sidebar.jsx
+++ peekaboo_admin_react/src/components/sidebar/Sidebar.jsx
@@ -1,45 +1,56 @@
-import "./sidebar.scss";
+//Style File
+import "../../index.scss";
+//React Router
+import { Link, useNavigate } from "react-router-dom";
+//Svg's
+import logo from "../../assets/PEEKABOO.svg"
+//Mui Icons
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
-import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
-import MyLocationIcon from '@mui/icons-material/MyLocation';
+import WrongLocationIcon from '@mui/icons-material/WrongLocation';
 import MapIcon from '@mui/icons-material/Map';
-import { Link } from "react-router-dom";
+
 const Sidebar = () => {
+  let navigate = useNavigate();
   return (
     <div className="sidebar">
       <div className="top">
-      <Link to="/" style={{ textDecoration: "none" }}>
-        <span className="logo">PEEKABOO</span>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <img src={logo} className="logo" />
         </Link>
       </div>
       <hr />
       <div className="center">
         <ul>
-
           <p className="title">MANAGE</p>
           <Link to="/users" style={{ textDecoration: "none" }}>
-          <li>
-            <PersonOutlineIcon className="icon" />
-            <span>Users</span>
-          </li>
+            <li>
+              <PersonOutlineIcon className="icon" />
+              <span>Users</span>
+            </li>
           </Link>
 
           <Link to="/map" style={{ textDecoration: "none" }}>
-          <li>
-            <MapIcon className="icon" />
-            <span>MAP</span>
-          </li>
+            <li>
+              <MapIcon className="icon" />
+              <span>Map</span>
+            </li>
+          </Link>
+          <Link to="/false_alarms" style={{ textDecoration: "none" }}>
+            <li>
+              <WrongLocationIcon className="icon" />
+              <span>False Infras</span>
+            </li>
           </Link>
 
-          <p className="title">USER</p>
-          <li>
-            <AccountCircleOutlinedIcon className="icon" />
-            <span>Profile</span>
-          </li>
-          <li>
+          <li className="logout"
+          onClick={() => {
+            localStorage.removeItem('token');
+            navigate("/login");
+          }
+          }>
             <ExitToAppIcon className="icon" />
-            <span>Logout</span>
+            <span >Logout</span>
           </li>
         </ul>
       </div>
