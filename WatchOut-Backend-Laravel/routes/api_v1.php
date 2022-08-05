df426776b131b0bc5c8660cea9c4f608f22b501b
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-08-05 08:54:53 +0300

    Backend : Renaming some routes , duplicating some (one for admin, one for userr), and general enhacement according to the recent needs to our frontends(and few routes for the recent created functions in the controllers)

--- WatchOut-Backend-Laravel/routes/api_v1.php
+++ WatchOut-Backend-Laravel/routes/api_v1.php
@@ -9,37 +9,55 @@ use App\Http\Controllers\Api\V1\Admin\AdminController;
 
 
 
-Route::post('/signup', [JWTAuthController::class, 'signup']);
+Route::group(['prefix' => 'user'] , function(){
+    
+        Route::post('/login', [JWTAuthController::class, 'login']);
+    
+        Route::post('/signup', [JWTAuthController::class, 'signup']);
 
-Route::post('/login', [JWTAuthController::class, 'login']);
+        Route::group(['middleware' => 'api'], function($router) {
 
-
-Route::group(['middleware' => 'api'], function($router) {
-
-    Route::get('/get_all_infras/{user_id?}', [CommonController::class, 'getAllInfras']);
-
-    Route::group(['prefix' => 'user'] , function(){
-
-        Route::post('/get_near_infras', [UserController::class, 'getNearInfras']);   //infras :infrastructural problems
+        //infras :infrastructural problems
+        Route::post('/get_near_infras', [UserController::class, 'getNearInfras']);   
 
         Route::put('/add_coins/{user_id}', [UserController::class, 'addCoins']);
 
-        Route::put('/false_alarm/{infra_id}', [UserController::class, 'falseAlarm']);
+        Route::put('/false_infra/{infra_id}', [UserController::class, 'falseInfra']);
         
         Route::put('/edit_profile', [UserController::class, 'editProfile']);
 
-        Route::post('/report', [UserController::class, 'report']);
+        Route::post('/report', [CommonController::class, 'report']);
 
         Route::post('/logout', [JWTAuthController::class, 'logout']);
+
+        Route::get('/get_all_infras/{user_id}', [CommonController::class, 'getAllInfras']);
+
+        Route::get('/false_infras/{user_id}', [CommonController::class, 'getUserFalseInfras']);
     });
+});
 
     Route::group(['prefix' => 'admin'] , function(){
        
+        Route::post('/login', [JWTAuthController::class, 'login']);
+
+        Route::group(['middleware' => 'api'], function($router) {
+
+        Route::post('/logout', [JWTAuthController::class, 'logout']);
+
         Route::put('/fix_infra/{infra_id}', [AdminController::class, 'fixInfra']);
+
+        Route::post('/report', [CommonController::class, 'report']);
         
-        Route::put('/delete_user/{user_id}', [AdminController::class, 'deleteInfra']);
+        Route::put('/reject_false_infra/{infra_id}', [AdminController::class, 'rejectFalseInfra']);
+        
+        Route::put('/delete_user/{user_id}', [AdminController::class, 'deleteUser']);
       
         Route::get('/users/{user_id?}', [AdminController::class, 'getAllUsers']);
+
+        Route::get('/false_infras/{infra_id?}', [AdminController::class, 'getFalseInfras']);
+
+        Route::get('/get_all_infras/{user_id}', [CommonController::class, 'getAllInfras']);
+
 
     });
 
