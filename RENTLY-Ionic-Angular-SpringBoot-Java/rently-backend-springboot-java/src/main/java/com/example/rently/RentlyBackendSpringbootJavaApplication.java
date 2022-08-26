1f64b3bc8196e5517452c2d1ab4479ec10253e6d
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-08-27 00:28:58 +0300

    Setting up Spring Boot as the backend framework

---
+++ RENTLY-Ionic-Angular-SpringBoot-Java/rently-backend-springboot-java/src/main/java/com/example/rently/RentlyBackendSpringbootJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.rently;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentlyBackendSpringbootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentlyBackendSpringbootJavaApplication.class, args);
+	}
+
+}
---
+++ RENTLY-Ionic-Angular-SpringBoot-Java/rently-backend-springboot-java/src/test/java/com/example/rently/RentlyBackendSpringbootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rently;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentlyBackendSpringbootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
