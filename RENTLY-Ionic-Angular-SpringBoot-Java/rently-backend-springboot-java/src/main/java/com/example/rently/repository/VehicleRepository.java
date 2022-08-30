63f6af2735524bf1db010af99ecba36cd372770c
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-08-30 23:23:05 +0300

    Backend: Fixing bug in VehicleRepository (by adding "@Repository" annotation )

--- RENTLY-Ionic-Angular-SpringBoot-Java/rently-backend-springboot-java/src/main/java/com/example/rently/repository/VehicleRepository.java
+++ RENTLY-Ionic-Angular-SpringBoot-Java/rently-backend-springboot-java/src/main/java/com/example/rently/repository/VehicleRepository.java
@@ -1,8 +1,10 @@
 package com.example.rently.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.rently.models.Vehicle;
+@Repository
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long>{
 
