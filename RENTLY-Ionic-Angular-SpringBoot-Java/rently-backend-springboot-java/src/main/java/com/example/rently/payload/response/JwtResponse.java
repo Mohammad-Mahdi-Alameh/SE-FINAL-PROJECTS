f8e8f4dfdef6bb0ba8ae8faed51df2760bbc912b
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-08-31 12:01:05 +0300

    Backend: Updating the "JwtResponse" class with the recent added attributes to the "User" entity class

--- RENTLY-Ionic-Angular-SpringBoot-Java/rently-backend-springboot-java/src/main/java/com/example/rently/payload/response/JwtResponse.java
+++ RENTLY-Ionic-Angular-SpringBoot-Java/rently-backend-springboot-java/src/main/java/com/example/rently/payload/response/JwtResponse.java
@@ -4,14 +4,24 @@ public class JwtResponse {
   private String token;
   private String type = "Bearer";
   private Long id;
+  private String firstname;
+  private String lastname;
   private String username;
   private String email;
+  private int phoneNumber;
+  private String picture;
+  private int isAdmin;
 
-  public JwtResponse(String accessToken, Long id, String username, String email) {
+  public JwtResponse(String accessToken, Long id, String firstname,String lastname, String username, String email, int phoneNumber ,String picture, int isAdmin) {
     this.token = accessToken;
     this.id = id;
+    this.firstname = firstname;
+    this.lastname = lastname;
     this.username = username;
     this.email = email;
+    this.phoneNumber = phoneNumber;
+    this.picture = picture;
+    this.isAdmin = isAdmin;
   }
 
   public String getAccessToken() {
@@ -53,5 +63,34 @@ public class JwtResponse {
   public void setUsername(String username) {
     this.username = username;
   }
-
+  public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+  public int getIsAdmin() {
+		return isAdmin;
+	}
+	public void setIsAdmin(int isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
 }
