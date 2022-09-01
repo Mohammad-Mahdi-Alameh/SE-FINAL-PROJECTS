c47d30bcad77b056292e48383fa186a60dcf6347
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-09-02 00:14:21 +0300

    Backend: Renaming "UserDetailsService" interface to "UserService" and adding "getUserById" method to it

--- RENTLY-Ionic-Angular-SpringBoot-Java/rently-backend-springboot-java/src/main/java/com/example/rently/security/services/UserService.java
+++ RENTLY-Ionic-Angular-SpringBoot-Java/rently-backend-springboot-java/src/main/java/com/example/rently/security/services/UserService.java
@@ -3,6 +3,12 @@ package com.example.rently.security.services;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public interface UserDetailsService {
+import com.example.rently.models.User;
+
+
+public interface UserService{
+    
     UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+
+    User getUserById(Long userId);
 }
