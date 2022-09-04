8b19a25e344481012147f97593ac96cff2a5cbba
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-09-04 03:49:40 +0300

    Frontend: Creating "auth" guard and then creating "user" route and applying the "auth" guard to its path (and making the "login" path empty)

--- RENTLY-Ionic-Angular-SpringBoot-Java/rently-user-frontend-ionic-angular/rently/src/app/app-routing.module.ts
+++ RENTLY-Ionic-Angular-SpringBoot-Java/rently-user-frontend-ionic-angular/rently/src/app/app-routing.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    path: 'user',
+    loadChildren: () =>
+      import('./tabs/tabs.module').then(m => m.TabsPageModule),
+    canActivate: [AuthGuard]
   },
   {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    path: '',
+    loadChildren: () =>
+      import('./login/login.module').then(m => m.LoginPageModule)
   },
   {
     path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+    loadChildren: () =>
+      import('./register/register.module').then(m => m.RegisterPageModule)
   }
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
