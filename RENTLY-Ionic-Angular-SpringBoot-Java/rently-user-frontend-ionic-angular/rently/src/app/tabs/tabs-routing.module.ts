0f69ce8dea8bf532f857f4fcc7df7b9cdd34e249
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-09-04 03:55:29 +0300

    Frontend: Modyfing the routing of the tabs

--- RENTLY-Ionic-Angular-SpringBoot-Java/rently-user-frontend-ionic-angular/rently/src/app/tabs/tabs-routing.module.ts
+++ RENTLY-Ionic-Angular-SpringBoot-Java/rently-user-frontend-ionic-angular/rently/src/app/tabs/tabs-routing.module.ts
@@ -4,36 +4,50 @@ import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
-    path: 'tabs',
+    path: '',
+    redirectTo: '/user/tab1',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
     component: TabsPage,
     children: [
       {
         path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+          }
+        ]
       },
       {
         path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+          }
+        ]
       },
       {
         path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
-      },
-      {
-        path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+          }
+        ]
       }
     ]
-  },
-  {
-    path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full'
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class TabsPageRoutingModule {}
