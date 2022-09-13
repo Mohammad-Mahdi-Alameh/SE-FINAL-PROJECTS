0a9e899fcb34c5197f009c79887cf8ea99913a1e
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-09-14 02:45:17 +0300

    Frontend: Creating a register() function in the register page that will call the register() function in the "Auth" service

--- RENTLY-Ionic-Angular-SpringBoot-Java/rently-user-frontend-ionic-angular/rently/src/app/pages/register/register.page.ts
+++ RENTLY-Ionic-Angular-SpringBoot-Java/rently-user-frontend-ionic-angular/rently/src/app/pages/register/register.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,13 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-  register:string="Register";
-  button:string="REGISTER";
-  registerCard:string="card";
+  user: any = {};
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private alertCtrl: AlertController
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-}
+  register() {
+    this.auth.register(this.user).subscribe(async (res) => {
+      if (res) {
+        this.router.navigateByUrl('');
+      } else {
+        const alert = await this.alertCtrl.create({
+          header: '',
+          message: 'Fill the missing fields !',
+          buttons: ['OK'],
+        });
+        await alert.present();
+      }
+    });
+  }}
