ad24e6e1658abec3d0efc5cd0e5a86ac1df5436f
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-09-13 15:17:05 +0300

    Frontend: Creating "sharedComponents" module that we be used to export and import the components in the pages later on

---
+++ RENTLY-Ionic-Angular-SpringBoot-Java/rently-user-frontend-ionic-angular/rently/src/app/components/shared-components.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IonicModule
+  ],
+  exports:[]
+})
+export class SharedComponentsModule { }
