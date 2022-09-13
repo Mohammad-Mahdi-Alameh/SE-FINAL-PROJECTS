e5f794f6c9f237ae89bda70cd8f1e5b0f6ba9089
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-09-14 00:25:08 +0300

    Frontend: Creating login function the in the "auth" service that calls the login api

--- RENTLY-Ionic-Angular-SpringBoot-Java/rently-user-frontend-ionic-angular/rently/src/app/services/auth/auth.service.ts
+++ RENTLY-Ionic-Angular-SpringBoot-Java/rently-user-frontend-ionic-angular/rently/src/app/services/auth/auth.service.ts
@@ -1,10 +1,37 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  public user: Observable<any>;
+  private userData = new BehaviorSubject(null);
 
-  constructor() {
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient, private router: Router) {
   }
+
+
+  login(credentials: {username: string, password: string }) {
+    return this.http.post<any>('http://localhost:8080/api/auth/signin',credentials, this.httpOptions).pipe(
+      catchError(this.handleError<any>('Error occured'))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }  
+  
+
 }
