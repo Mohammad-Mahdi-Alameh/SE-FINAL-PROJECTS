3d3700de49c0d8115fde6ade71951e77e473963a
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-09-13 15:20:12 +0300

    Frontend: Creating "Navbar" component and importing it in the shared components module

---
+++ RENTLY-Ionic-Angular-SpringBoot-Java/rently-user-frontend-ionic-angular/rently/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent implements OnInit {
+  @Input() title?: string;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- RENTLY-Ionic-Angular-SpringBoot-Java/rently-user-frontend-ionic-angular/rently/src/app/components/shared-components.module.ts
+++ RENTLY-Ionic-Angular-SpringBoot-Java/rently-user-frontend-ionic-angular/rently/src/app/components/shared-components.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule,
     IonicModule
   ],
-  exports:[]
+  exports:[NavbarComponent]
 })
 export class SharedComponentsModule { }
