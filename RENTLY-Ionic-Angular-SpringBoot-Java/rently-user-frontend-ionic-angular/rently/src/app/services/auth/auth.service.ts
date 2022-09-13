bbb38309a96ab86752eb1371b0820b51c4055990
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-09-14 02:43:24 +0300

    Frontend: Creating register function the in the "auth" service that calls the register api

--- RENTLY-Ionic-Angular-SpringBoot-Java/rently-user-frontend-ionic-angular/rently/src/app/services/auth/auth.service.ts
+++ RENTLY-Ionic-Angular-SpringBoot-Java/rently-user-frontend-ionic-angular/rently/src/app/services/auth/auth.service.ts
@@ -3,26 +3,47 @@ import { BehaviorSubject, Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
-
+export class UserInformation{
+  firstname : string;
+  lastname : string;
+  email : string;
+  username : string;
+  password : string;
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   public user: Observable<any>;
   private userData = new BehaviorSubject(null);
 
+
+
   httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
 
-  constructor(private http: HttpClient, private router: Router) {
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(credentials: { username: string; password: string }) {
+    return this.http
+      .post<any>(
+        'http://localhost:8080/api/auth/signin',
+        credentials,
+        this.httpOptions
+      )
+      .pipe(catchError(this.handleError<any>('Error occured')));
   }
 
+  register(user:UserInformation){
+    return this.http
+    .post<any>(
+      'http://localhost:8080/api/auth/signup',
+      user,
+      this.httpOptions
+    )
+    .pipe(catchError(this.handleError<any>('Username/Email already exist!')));
 
-  login(credentials: {username: string, password: string }) {
-    return this.http.post<any>('http://localhost:8080/api/auth/signin',credentials, this.httpOptions).pipe(
-      catchError(this.handleError<any>('Error occured'))
-    );
   }
 
   getUser() {
@@ -30,19 +51,16 @@ export class AuthService {
   }
 
   logout() {
-    localStorage.removeItem("token");
-      this.router.navigateByUrl('/');
-      this.userData.next(null);
-    };
-  
-  
+    localStorage.removeItem('token');
+    this.router.navigateByUrl('/');
+    this.userData.next(null);
+  }
+
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.error(error);
       console.log(`${operation} failed: ${error.message}`);
       return of(result as T);
     };
-  }  
-  
-
+  }
 }
